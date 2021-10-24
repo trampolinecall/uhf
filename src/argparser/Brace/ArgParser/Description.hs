f0b8a4f08d0d127362b64f6a9a7d66ab1a21ad31
Jason N <fireworkinyourbathtub@gmail.com>
2021-10-24 12:12:33 -0400

    add argument parser code

--- src/argparser/Brace/ArgParser/Description.hs
+++ src/argparser/Brace/ArgParser/Description.hs
@@ -1,14 +1,29 @@
 module Brace.ArgParser.Description
     ( Description(..)
 
-    , Arg(..)
+    , HelpMessage(..)
+    , ArgProps(..)
 
+    , ShortFlagName(..)
+    , LongFlagName(..)
+    , FlagProps(..)
+
+    , ValueName(..)
     , NValues(..)
+    , TakesValueProps(..)
+
+    , Arg(..)
 
     , positional
     , flag
     , option
+
+    , get_flag
+    , get_positionals
     ) where
+
+import Data.List
+import Data.Maybe
 
 data Description = Description [Arg]
 
@@ -35,3 +50,27 @@ flag flag_ch flag_name help_message = Flag (FlagProps (ShortFlagName flag_ch) (L
 
 option :: Char -> Maybe String -> String -> String -> NValues -> Arg
 option option_ch option_name help_message value_name n_values = Flag (FlagProps (ShortFlagName option_ch) (LongFlagName <$> option_name)) (ArgProps $ HelpMessage help_message) (Just $ TakesValueProps (ValueName value_name) n_values)
+
+get_flag :: Either Char String -> Description -> Maybe (FlagProps, ArgProps, Maybe TakesValueProps)
+get_flag name (Description args) =
+    let predicate =
+            case name of
+                Left ch ->
+                    let check (Flag (FlagProps (ShortFlagName ch') _) _ _) = ch' == ch
+                        check _ = False
+                    in check
+
+                Right str ->
+                    let check (Flag (FlagProps _ (Just (LongFlagName str'))) _ _) = str' == str
+                        check _ = False
+                    in check
+
+    in case find predicate args of
+        Just (Flag fp ap m_tvp) -> Just (fp, ap, m_tvp)
+        _ -> Nothing
+
+get_positionals :: Description -> [(ArgProps, TakesValueProps)]
+get_positionals (Description args) =
+    let p (Positional ap tvp) = Just (ap, tvp)
+        p _ = Nothing
+    in mapMaybe p args

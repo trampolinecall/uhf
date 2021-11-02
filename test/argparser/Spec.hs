08d053c13ab5b71fc5178fb0b7ba8e5b48c54b58
Jason N <fireworkinyourbathtub@gmail.com>
2021-11-02 11:42:20 -0400

    move help stuff to new file, return ArgMatches

--- test/argparser/Spec.hs
+++ test/argparser/Spec.hs
@@ -4,10 +4,12 @@ import Test.HUnit
 
 import qualified Brace.ArgParser as ArgParser (tests)
 import qualified Brace.ArgParser.Parser as Parser (tests)
+import qualified Brace.ArgParser.Help as Help (tests)
 
 main :: IO ()
 main =
     runTestTTAndExit $ test
             [ "ArgParser" ~: ArgParser.tests
             , "ArgParser.Parser" ~: Parser.tests
+            , "ArgParser.Help" ~: Help.tests
             ]

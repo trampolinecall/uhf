1a30eaad50e9b370b5b8f33d0a5482c7d743fff2
Jason N <fireworkinyourbathtub@gmail.com>
2021-11-02 12:08:59 -0400

    add argument parser to uhfc and uhfi

--- src/interpreter/Main.hs
+++ src/interpreter/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.Environment (getArgs, getProgName)
+
+import Brace.ArgParser
+
+args_description :: Description
+args_description = Description
+    [ positional "file to run" "FILE" (Number 1)
+    ]
+
 main :: IO ()
-main = putStrLn "uhfi"
+main =
+    getProgName >>= \ prog_name ->
+    getArgs >>= \ args ->
+    get_matches args_description prog_name args >>= \ matches ->
+    report_errors (get_positional_result_1 "FILE" matches) >>= \ file ->
+    putStrLn (show file)

3f70502a6e009228c492880f76337f3e44d9dda8
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-02 12:08:59 -0400

    add argument parser to uhfc and uhfi

--- src/compiler/Main.hs
+++ src/compiler/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.Environment (getArgs, getProgName)
+
+import Brace.ArgParser
+
+args_description :: Description
+args_description = Description
+    [ positional "files to compile" "FILES" OneOrMore
+    ]
+
 main :: IO ()
-main = putStrLn "uhfc"
+main =
+    getProgName >>= \ prog_name ->
+    getArgs >>= \ args ->
+    get_matches args_description prog_name args >>= \ matches ->
+    report_errors (get_positional_result_ne "FILES" matches) >>= \ files ->
+    putStrLn (show files)

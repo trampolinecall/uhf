599db99b1732206326897d8598890917ec80a59b
trampolinecall <>
2021-11-03 17:23:09 -0400

    add more helper functions for lexer

--- src/io/UHF/IO/File.hs
+++ src/io/UHF/IO/File.hs
@@ -3,7 +3,7 @@ module UHF.IO.File where
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 
-data File = File { path :: String, contents :: Text.Text }
+data File = File { path :: String, contents :: Text.Text } deriving (Eq, Show)
 
 open_file :: String -> IO File
 open_file path' = File path' <$> Text.IO.readFile path'

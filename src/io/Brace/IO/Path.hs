4d6de08151c204cccadfb328701cbb733a492e3f
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-03 14:50:52 -0400

    add Path module to io library

---
+++ src/io/Brace/IO/Path.hs
@@ -0,0 +1,17 @@
+module Brace.IO.Path where
+
+import Test.HUnit
+
+replace_extension :: String -> String -> String
+replace_extension ext p =
+    let dropped = dropWhile (/= '.') $ reverse p
+    in if null dropped
+        then p ++ "." ++ ext
+        else reverse dropped ++ ext
+
+
+tests :: Test
+tests = test
+    [ "thing.def" ~=? replace_extension "def" "thing.abc"
+    , "thing.def" ~=? replace_extension "def" "thing"
+    ]

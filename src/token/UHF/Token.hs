c32ffec08203980abc4548fd9b9bbfc4571c81f3
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-03 15:43:43 -0400

    add token library

---
+++ src/token/UHF/Token.hs
@@ -0,0 +1,37 @@
+module UHF.Token
+    ( Token(..)
+    ) where
+
+data Token
+    = OParen
+    | CParen
+    | OBrack
+    | CBrack
+    | Comma
+    | Equal
+    | DoubleColon
+
+    | AlphaIdentifier String
+    | SymbolIdentifier String
+
+    | CharLit Char
+    | StringLit String
+    | IntLit IntLitBase Integer
+    | FLoatLit Double
+    | BoolLit Bool
+
+    | Root
+    | Let
+    | Data
+    | Under
+    | If
+    | Else
+    | Case
+
+    | OBrace
+    | CBrace
+    | Semicolon
+    | Indent
+    | Dedent
+    | Newline
+    deriving Eq

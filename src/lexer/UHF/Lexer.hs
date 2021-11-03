49857ce5cb2b3c41fb9edabdb884b15d2ef064ea
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-03 16:13:39 -0400

    define lexer helper functions

--- src/lexer/UHF/Lexer.hs
+++ src/lexer/UHF/Lexer.hs
@@ -18,8 +18,14 @@ data IndentFrame
     = IndentationSensitive Int
     | IndentationInsensitive
 
+l_contents :: Lexer -> Text.Text
+l_contents = File.contents . source_file
+
 remaining :: Lexer -> Text.Text
-remaining = _
+remaining l = Text.drop (source_location l) (l_contents l)
+
+passed :: Lexer -> Text.Text
+passed l = Text.take (source_location l) (l_contents l)
 
 rev_passed :: Lexer -> Text.Text
-rev_passed = _
+rev_passed = Text.reverse . passed

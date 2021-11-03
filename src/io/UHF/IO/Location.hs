eb0a156fadcc47ef9741837bd32696caeff8b8d8
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-03 17:23:09 -0400

    add more helper functions for lexer

--- src/io/UHF/IO/Location.hs
+++ src/io/UHF/IO/Location.hs
@@ -2,8 +2,8 @@ module UHF.IO.Location where
 
 import qualified UHF.IO.File as File
 
-data Location = Location { file :: File.File, row :: Int, col :: Int }
-data Span = Span { start :: Location, end :: Location }
+data Location = Location { file :: File.File, row :: Int, col :: Int } deriving (Eq, Show)
+data Span = Span { start :: Location, before_end :: Location, end :: Location } deriving (Eq, Show)
 
 data Located a = Located Span a
 

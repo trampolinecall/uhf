2e45c4aab3481dd7b6e3066f6ace8d38eb4f792b
Jason N <fireworkinyourbathtub@gmail.com>
2021-11-10 15:53:16 -0500

    implement most of lexer, lots of lexer tests

--- src/io/UHF/IO/File.hs
+++ src/io/UHF/IO/File.hs
@@ -3,7 +3,10 @@ module UHF.IO.File where
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 
-data File = File { path :: String, contents :: Text.Text } deriving (Eq, Show)
+data File = File { path :: String, contents :: Text.Text } deriving (Eq)
+
+instance Show File where
+    show (File p _) = "File { path = " ++ show p ++ ", contents = ... }"
 
 open_file :: String -> IO File
 open_file path' = File path' <$> Text.IO.readFile path'

4ddf634c72d514324ec466d7634c2a2c6e4ffe47
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-10 18:00:38 -0500

    add DFA module to lexer

--- test/lexer/Spec.hs
+++ test/lexer/Spec.hs
@@ -3,9 +3,11 @@ module Main where
 import Test.HUnit
 
 import qualified UHF.Lexer as Lexer
+import qualified UHF.Lexer.DFA as DFA
 
 main :: IO ()
 main =
     runTestTTAndExit $ test
         [ "Lexer" ~: Lexer.tests
+        , "DFA" ~: DFA.tests
         ]

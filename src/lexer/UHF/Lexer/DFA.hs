ae4d7ec4defd212fae5fca9cfbad6278787c42af
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-10 20:24:58 -0500

    move changing result to transitions

--- src/lexer/UHF/Lexer/DFA.hs
+++ src/lexer/UHF/Lexer/DFA.hs
@@ -14,9 +14,9 @@ import Test.HUnit (Test, test, (~:), (~=?))
 
 import qualified Data.Text as Text
 
-data DFA r = DFA [State r] (Maybe Char -> Transition)
-data State r = State { state_transition :: (Maybe Char -> Transition), state_res_mod :: (r -> Maybe Char -> r) }
-data Transition = StateNum Int | Reject | Accept
+data DFA r = DFA [State r] (r -> Maybe Char -> Transition r)
+data State r = State { state_transition :: (r -> Maybe Char -> Transition r) }
+data Transition r = StateNum Int r | Accept r | Reject
 
 data CurState = Begin | CurStateNum Int
 
@@ -24,30 +24,23 @@ run_dfa :: DFA r -> Text.Text -> r -> Maybe (Int, r)
 run_dfa dfa = run_dfa' dfa Begin 0
 
 run_dfa' :: DFA r -> CurState -> Int -> Text.Text -> r -> Maybe (Int, r)
-run_dfa' dfa@(DFA states begin_transition) cur_state cur_len_consumed input res =
+run_dfa' dfa@(DFA states begin_transition) cur_state cur_len_consumed input cur_res =
     let cur_transition = case cur_state of
             Begin -> begin_transition
             CurStateNum i -> state_transition $ states !! i
 
-        res_mod =  case cur_state of
-            Begin -> const
-            CurStateNum i -> state_res_mod $ states !! i
-
         cur_char = fst <$> Text.uncons input
 
-        transition = cur_transition cur_char
-
-    in case transition of
+    in case cur_transition cur_res cur_char of
         Reject -> Nothing
-        Accept -> Just (cur_len_consumed, res)
+        Accept res -> Just (cur_len_consumed, res)
 
-        StateNum next_state ->
+        StateNum next_state next_res ->
             let (next_len_consumed, next_input) =
                     case Text.uncons input of
                         Just (_, more) -> (cur_len_consumed + 1, more)
                         Nothing -> (cur_len_consumed, input)
 
-                next_res = res_mod res cur_char
             in run_dfa' dfa (CurStateNum next_state) next_len_consumed next_input next_res
 
 tests :: Test
@@ -55,23 +48,21 @@ tests = test
     [ "run_dfa" ~:
         let dfa = DFA
               [ State
-                  (\case
-                      Just 'a' -> StateNum 0
-                      Just 'b' -> StateNum 1
+                  (\ r -> \case
+                      Just 'a' -> StateNum 0 (r + 1)
+                      Just 'b' -> StateNum 1 r
                       _ -> Reject
                   )
-                  (\ _ _ -> 2)
               , State
-                  (\case
-                      Just 'b' -> StateNum 1
-                      Nothing -> Accept
+                  (\ r -> \case
+                      Just 'b' -> StateNum 1 r
+                      Nothing -> Accept r
                       _ -> Reject
                   )
-                  (\ _ _ -> 3)
               ]
-              (\ c -> if c == Just 'a' then StateNum 0 else Reject)
+              (\ r c -> if c == Just 'a' then StateNum 0 r else Reject)
         in
-            [ Just (6, 3 :: Int) ~=? run_dfa dfa "aaaabb" 0
+            [ Just (4, 3 :: Int) ~=? run_dfa dfa "aaaabb" 0
             , Nothing ~=? run_dfa dfa "" 0
             , Nothing ~=? run_dfa dfa "aa" 0
             , Nothing ~=? run_dfa dfa "aba" 0

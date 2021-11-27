676f03a49c2d8e4293620fbd77284096579cafad
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-26 19:18:45 -0500

    add post processing phase to lexer

--- test/lexer.hs
+++ test/lexer.hs
@@ -2,8 +2,9 @@ module Main where
 
 import Test.Tasty
 
-import qualified UHF.Lexer (tests)
+import qualified UHF.Lexer.MainLexer (tests)
+import qualified UHF.Lexer.PostProcess (tests)
 import qualified UHF.Lexer.DFA (tests)
 
 main :: IO ()
-main = defaultMain $ testGroup "lexer" [UHF.Lexer.tests, UHF.Lexer.DFA.tests]
+main = defaultMain $ testGroup "lexer" [UHF.Lexer.MainLexer.tests, UHF.Lexer.PostProcess.tests, UHF.Lexer.DFA.tests]

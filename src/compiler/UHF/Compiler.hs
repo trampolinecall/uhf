af6fdad52d19d8c2592824e79c8e2cb0fccdec30
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-28 21:27:15 -0500

    move LexError to separate file, add IsDiagnostic, add instance of IsDiagnostic

--- src/compiler/UHF/Compiler.hs
+++ src/compiler/UHF/Compiler.hs
@@ -2,7 +2,22 @@ module UHF.Compiler
     ( compile
     ) where
 
-import qualified Data.Text as Text
+import qualified UHF.IO.File as File
+import qualified UHF.Diagnostic as Diagnostic
+import qualified UHF.Lexer as Lexer
 
-compile :: Text.Text -> ()
-compile _ = () -- TODO
+compile :: Phase File.File (Maybe ())
+compile = lex_phase
+
+type Phase a b = a -> ([Diagnostic.Diagnostic], b)
+
+link_phases :: (a -> ([Diagnostic.Diagnostic], b)) -> (b -> ([Diagnostic.Diagnostic], c)) -> a -> ([Diagnostic.Diagnostic], c)
+link_phases phase1 phase2 a =
+    let (diags1, b) = phase1 a
+        (diags2, c) = phase2 b
+    in (diags1 ++ diags2, c)
+
+lex_phase :: Phase File.File (Maybe ())
+lex_phase file =
+    let (diagnostics, _) = Lexer.lex file
+    in (map Diagnostic.to_diagnostic diagnostics, Nothing)

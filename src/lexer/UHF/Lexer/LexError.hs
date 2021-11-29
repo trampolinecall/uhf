b1dfb28fb8376f7946c2b72401843fb2e59e40bd
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-11-28 21:27:22 -0500

    add missing file

---
+++ src/lexer/UHF/Lexer/LexError.hs
@@ -0,0 +1,24 @@
+module UHF.Lexer.LexError
+    ( LexError(..)
+    ) where
+
+import qualified UHF.IO.Location as Location
+import qualified UHF.Diagnostic as Diagnostic
+
+data LexError
+    = BadChar Char Location.Span
+    | UnclosedMComment Location.Span
+    | UnclosedStrLit Location.Span
+    | UnclosedCharLit Location.Span
+    | MulticharCharLit Location.Span
+    | InvalidIntBase Char Location.Span
+    | InvalidIntDigit Char Location.Span
+    | NonDecimalFloat Location.Span
+    | MissingDigits Location.Span
+    -- TODO: add 4 fields: new indent level, before indent level, two closest indentation levels
+    | BadDedent Location.Span
+    | InvalidDoubleColon Location.Span
+    deriving (Eq, Show)
+
+instance Diagnostic.IsDiagnostic LexError where
+    to_diagnostic = undefined -- TODO

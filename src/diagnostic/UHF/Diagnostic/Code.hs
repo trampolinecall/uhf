25a3d503a5a979efc6ea5e8bfbceaca7a16e9429
Jason N <fireworkinyourbathtub@gmail.com>
2021-12-03 20:25:56 -0500

    add template haskell for autogenerating diagnostic codes

--- src/diagnostic/UHF/Diagnostic/Code.hs
+++ src/diagnostic/UHF/Diagnostic/Code.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE DeriveLift #-}
+
 module UHF.Diagnostic.Code
     ( Code(..)
     , Type(..)
     ) where
 
 import qualified Data.Text as Text
+import qualified Language.Haskell.TH.Syntax as Syntax
 
-data Type = Error | Warning | DebugMessage | InternalError
+data Type = Error | Warning | DebugMessage | InternalError deriving Syntax.Lift
 data Code =
     Code
     { code_type :: Type
     , code_code_desc :: Maybe (Text.Text, Text.Text)
     }
+    deriving Syntax.Lift

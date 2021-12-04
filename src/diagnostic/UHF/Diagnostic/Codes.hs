4cfe801530c20cd7837cb0f13fd439adbfb95217
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-12-03 20:31:44 -0500

    begin implementing to_diagnostic for LexError

--- src/diagnostic/UHF/Diagnostic/Codes.hs
+++ src/diagnostic/UHF/Diagnostic/Codes.hs
@@ -6,3 +6,13 @@ import UHF.Diagnostic.Code
 import UHF.Diagnostic.CodeTH
 
 $(code Error 0 "bad-char")
+$(code Error 1 "unclosed-multiline-comment")
+$(code Error 2 "invalid-int-base")
+$(code Error 3 "invalid-int-digit")
+$(code Error 4 "non-decimal-float")
+$(code Error 5 "missing-digits")
+$(code Error 6 "unclosed-string-lit")
+$(code Error 7 "unclosed-char-lit")
+$(code Error 8 "invalid-char-lit")
+$(code Error 9 "bad-dedent")
+$(code Error 10 "invalid-double-colon")

f0b3ce5808bd33b3c7076df0c49a561f1335b913
Jason N <fireworkinyourbathtub@gmail.com>
2021-12-03 20:31:44 -0500

    begin implementing to_diagnostic for LexError

--- src/lexer/UHF/Lexer/LexError.hs
+++ src/lexer/UHF/Lexer/LexError.hs
@@ -4,7 +4,7 @@ module UHF.Lexer.LexError
 
 import qualified UHF.IO.Location as Location
 import qualified UHF.Diagnostic as Diagnostic
-import qualified UHF.Diagnostic.Codes as Diagnostic.Codes
+import qualified UHF.Diagnostic.Codes as Codes
 
 data LexError
     = BadChar Char Location.Span
@@ -22,4 +22,14 @@ data LexError
     deriving (Eq, Show)
 
 instance Diagnostic.IsDiagnostic LexError where
-    to_diagnostic = undefined -- TODO
+    to_diagnostic (BadChar ch sp) = Diagnostic.Diagnostic Codes.bad_char (Just sp) undefined
+    to_diagnostic (UnclosedMComment sp) = Diagnostic.Diagnostic Codes.unclosed_multiline_comment (Just sp) undefined
+    to_diagnostic (UnclosedStrLit sp) = Diagnostic.Diagnostic Codes.unclosed_string_lit (Just sp) undefined
+    to_diagnostic (UnclosedCharLit sp) = Diagnostic.Diagnostic Codes.unclosed_char_lit (Just sp) undefined
+    to_diagnostic (MulticharCharLit sp) = Diagnostic.Diagnostic Codes.invalid_char_lit (Just sp) undefined
+    to_diagnostic (InvalidIntBase ch sp) = Diagnostic.Diagnostic Codes.invalid_int_base (Just sp) undefined
+    to_diagnostic (InvalidIntDigit ch sp) = Diagnostic.Diagnostic Codes.invalid_int_digit (Just sp) undefined
+    to_diagnostic (NonDecimalFloat sp) = Diagnostic.Diagnostic Codes.non_decimal_float (Just sp) undefined
+    to_diagnostic (MissingDigits sp) = Diagnostic.Diagnostic Codes.missing_digits (Just sp) undefined
+    to_diagnostic (BadDedent sp) = Diagnostic.Diagnostic Codes.bad_dedent (Just sp) undefined
+    to_diagnostic (InvalidDoubleColon sp) = Diagnostic.Diagnostic Codes.invalid_double_colon (Just sp) undefined

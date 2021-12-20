1fecf4cf1d656398851a57f3a8b9699b448059f1
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2021-12-20 16:30:39 -0500

    change line to an adt

--- src/diagnostic/UHF/Diagnostic/Line.hs
+++ src/diagnostic/UHF/Diagnostic/Line.hs
@@ -1,6 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module UHF.Diagnostic.Line where
+module UHF.Diagnostic.Line
+    ( Line
+    , prefix
+    , separ
+    , contents
+
+    , file_line
+    , elipsis_line
+    , numbered_line
+    , other_line
+
+    , compare_line
+    , compare_many_lines
+    , compare_many_lines'
+
+    ) where
 
 import Test.Tasty.HUnit
 
@@ -12,17 +27,23 @@ import qualified UHF.IO.File as File
 import qualified System.Console.ANSI as ANSI
 import qualified Data.Text as Text
 
-type Line = (Text.Text, Char, FormattedString.FormattedString)
+data Line = Line { prefix :: Text.Text, separ :: Char, contents :: FormattedString.FormattedString } deriving (Show, Eq)
 
 file_line :: File.File -> Line
-file_line f = ("", '>', FormattedString.make_formatted_string [(Colors.file_path, Text.pack $ File.path f)])
+file_line f = Line "" '>' (FormattedString.make_formatted_string [(Colors.file_path, Text.pack $ File.path f)])
 
 elipsis_line :: Line
-elipsis_line = ("...", '|', FormattedString.make_formatted_string [([], "...")])
+elipsis_line = Line "..." '|' (FormattedString.make_formatted_string [([], "...")])
+
+numbered_line :: Int -> FormattedString.FormattedString -> Line
+numbered_line num = Line (Text.pack $ show num) '|'
+
+other_line :: FormattedString.FormattedString -> Line
+other_line = Line "" '|'
 
 compare_line :: Text.Text -> Char -> [(Char, [ANSI.SGR])] -> String -> String -> Line -> Bool
-compare_line prefix sep bindings text sgrs (line_prefix, line_sep, line_after) =
-    prefix == line_prefix &&
+compare_line pre sep bindings text sgrs (Line line_pre line_sep line_after) =
+    pre == line_pre &&
     sep == line_sep &&
     FormattedString.compare_formatted_string bindings text sgrs line_after
 
@@ -34,14 +55,14 @@ compare_many_lines bindings line_expectations =
 compare_many_lines' :: [(Char, [ANSI.SGR])] -> [(Text.Text, Char, String, String)] -> [Line] -> Assertion
 compare_many_lines' bindings line_expectations lns =
     let expectations_str = concatMap
-            (\ (prefix, ch, text, formats) ->
-                Text.unpack prefix ++ " " ++ [ch] ++ " " ++ show text ++ "\n" ++ replicate (Text.length prefix + 3) ' ' ++ show formats ++ "\n")
+            (\ (pre, ch, text, formats) ->
+                Text.unpack pre ++ " " ++ [ch] ++ " " ++ show text ++ "\n" ++ replicate (Text.length pre + 3) ' ' ++ show formats ++ "\n")
             line_expectations
 
         lns_str = concatMap
-            (\ (prefix, ch, fmstr) ->
+            (\ (Line pre ch fmstr) ->
                 let (fmstr_text, fmstr_formats) = FormattedString.formatted_string_contents_and_formats bindings fmstr
-                in Text.unpack prefix ++ " " ++ [ch] ++ " " ++ show fmstr_text ++ "\n" ++ replicate (Text.length prefix + 3) ' ' ++ show fmstr_formats ++ "\n"
+                in Text.unpack pre ++ " " ++ [ch] ++ " " ++ show fmstr_text ++ "\n" ++ replicate (Text.length pre + 3) ' ' ++ show fmstr_formats ++ "\n"
             )
             lns
 

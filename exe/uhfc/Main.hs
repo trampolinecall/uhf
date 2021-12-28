db6751e8169d10294faa6478d8eb4f8db19544d0
trampolinecall <>
2021-12-28 10:38:12 -0500

    implement compiler entry point

--- exe/uhfc/Main.hs
+++ exe/uhfc/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import UHF.ArgParser
+import qualified UHF.Compiler as Compiler
+import qualified UHF.IO.File as File
+import qualified UHF.Diagnostic as Diagnostic
+
+import qualified System.IO as IO
+
+import qualified Data.List.NonEmpty as NonEmpty
 
 args_description :: Description
 args_description = Description
@@ -11,4 +18,10 @@ main :: IO ()
 main =
     get_matches_io args_description >>= \ matches ->
     report_errors (get_positional_result_ne "FILES" matches) >>= \ files ->
-    putStrLn (show files)
+    sequence_ (NonEmpty.map compile files)
+
+compile :: String -> IO ()
+compile fname =
+    File.open_file fname >>= \ f ->
+    let (diags, res) = Compiler.compile f
+    in Diagnostic.report_diagnostics IO.stderr diags

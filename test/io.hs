6aa4fe170dd8685285b21ef937b36e50164027c2
trampolinecall <>
2021-12-29 12:13:25 -0500

    refactor location and span constructors

--- test/io.hs
+++ test/io.hs
@@ -2,7 +2,8 @@ module Main where
 
 import Test.Tasty
 
+import qualified UHF.IO.Location (tests)
 import qualified UHF.IO.Path (tests)
 
 main :: IO ()
-main = defaultMain $ testGroup "io" [UHF.IO.Path.tests]
+main = defaultMain $ testGroup "io" [UHF.IO.Location.tests, UHF.IO.Path.tests]

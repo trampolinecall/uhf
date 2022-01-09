ac4acc82389dd3dffdabd053448127c7f9f1f2af
trampolinecall <>
2022-01-09 17:15:35 -0500

    add asts

---
+++ src/ast/UHF/AST/Decl.hs
@@ -0,0 +1,8 @@
+module UHF.AST.Decl where
+
+import qualified UHF.AST.Type as Type
+import qualified UHF.AST.Expr as Expr
+
+data Decl
+    = TypeSignature String Type.Type
+    | Binding String Expr.Expr

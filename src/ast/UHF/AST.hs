c5f48b073d1d21c75b948d8e099928b790d96006
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2022-01-09 17:15:35 -0500

    add asts

--- src/ast/UHF/AST.hs
+++ src/ast/UHF/AST.hs
@@ -1,1 +1,9 @@
-module UHF.AST where
+module UHF.AST
+    ( module Decl
+    , module Expr
+    , module Type
+    ) where
+
+import qualified UHF.AST.Decl as Decl
+import qualified UHF.AST.Expr as Expr
+import qualified UHF.AST.Type as Type

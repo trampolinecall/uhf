4748c37f59bad6548dc1330ef04f8c2331a28d9e
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2022-04-12 11:49:39 -0400

    start parser

--- src/token/UHF/Token.hs
+++ src/token/UHF/Token.hs
@@ -1,16 +1,27 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module UHF.Token
     ( Token(..)
     , IntLitBase(..)
+
+    , Decimal(..)
     ) where
 
 import qualified Data.Decimal as Decimal
+import qualified Data.Data as Data
+
+deriving instance Data.Data Decimal.Decimal -- TODO: maybe not have an orphan instance
+newtype Decimal = Decimal Decimal.Decimal deriving (Show, Eq, Data.Data)
 
 data IntLitBase
     = Dec
     | Oct
     | Hex
     | Bin
-    deriving (Show, Eq)
+    deriving (Show, Eq, Data.Data)
 
 data Token
     = OParen
@@ -36,7 +47,7 @@ data Token
     | CharLit Char
     | StringLit String
     | IntLit IntLitBase Integer
-    | FloatLit Decimal.Decimal
+    | FloatLit Decimal
     | BoolLit Bool
 
     | OBrace
@@ -45,4 +56,4 @@ data Token
     | Indent
     | Dedent
     | Newline
-    deriving (Show, Eq)
+    deriving (Show, Eq, Data.Data)

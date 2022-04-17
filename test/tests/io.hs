93082a18a917e432dfc1647b227c4c5c935f2802
Jason N <fireworkinyourbathtub@gmail.com>
2022-04-17 12:07:31 -0400

    merge all internal libraries into one library, merge test executables

--- test/tests/io.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Test.Tasty
-
-import qualified UHF.IO.Location (tests)
-import qualified UHF.IO.Path (tests)
-
-main :: IO ()
-main = defaultMain $ testGroup "io" [UHF.IO.Location.tests, UHF.IO.Path.tests]

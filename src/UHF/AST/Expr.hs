dc7796386601f3f1612bb5f2d6e49aa8dbca2d16
trampolinecall <>
2022-10-04 17:37:29 -0400

    use Rational instead of Decimal

--- src/UHF/AST/Expr.hs
+++ src/UHF/AST/Expr.hs
@@ -1,12 +1,10 @@
 module UHF.AST.Expr where
 
-import qualified UHF.Token as Token
-
 data Expr
     = Identifier [String]
     | CharLit Char
     | StringLit String
     | IntLit Integer
-    | FloatLit Token.Decimal
+    | FloatLit Rational
     | BoolLit Bool
     deriving (Eq, Show)

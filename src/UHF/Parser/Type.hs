562c1cab2287c4d9446ef586cd6261a10c08ba1a
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-01-19 14:58:42 -0500

    split parser into many files, implement PEG tests

---
+++ src/UHF/Parser/Type.hs
@@ -0,0 +1,26 @@
+module UHF.Parser.Type(type_) where
+
+import UHF.Util.Prelude
+
+import qualified UHF.Parser.PEG as Parser
+import qualified UHF.Parser.ParseError as ParseError
+
+import qualified UHF.IO.Location as Location
+import qualified UHF.IO.File as File
+import qualified UHF.IO.Location.SpanHelper as SpanHelper
+
+import qualified UHF.Token as Token
+
+import qualified UHF.AST as AST
+
+import qualified Data.InfList as InfList
+
+import qualified Data.List as List
+import qualified Data.List.NonEmpty as NonEmpty
+
+import qualified Control.Monad.Trans.State as State
+
+type_ :: Parser.Parser AST.Type
+type_ =
+    Parser.consume "type" (Token.AlphaIdentifier ()) >>= \ (Location.Located _ (Token.AlphaIdentifier iden)) ->
+    pure (AST.Type'Identifier iden)

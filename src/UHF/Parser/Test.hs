83b14a21cd61d86b32a62246aa65ccd5f5bb440d
trampolinecall <>
2023-01-20 20:18:51 -0500

    remove type signatures

--- src/UHF/Parser/Test.hs
+++ src/UHF/Parser/Test.hs
@@ -23,6 +23,7 @@ import qualified Data.List.NonEmpty as NonEmpty
 import qualified Control.Monad.Trans.State as State
 
 data ParsingTest = forall r. (Show r, Eq r) => ParsingTest [Char] (File.File, PEG.TokenStream) r [([Char], PEG.Parser r)]
+
 make_token_stream :: [(Text, Token.NormalToken)] -> (File.File, PEG.TokenStream)
 make_token_stream things =
     let (file, things') = SpanHelper.make_spans_with_items things

c066296d52b8c5a6ae86bab2d57fec75512bd414
trampolinecall <>
2023-02-05 15:22:17 -0500

    reorganize lots of code and add arenas to ir

--- src/ir/UHF/IR/Value.hs
+++ src/ir/UHF/IR/Value.hs
@@ -2,7 +2,16 @@ module UHF.IR.Value where
 
 import UHF.Util.Prelude
 
+import qualified Arena
+
 import qualified UHF.IR.Expr as Expr
+
+data Key = Key Int deriving Show
+instance Arena.Key Key where
+    make_key = Key
+    unmake_key (Key i) = i
+
+type ValueArena value_identifier = Arena.Arena (Value value_identifier) Key
 
 data Value identifier = Value (Expr.Expr identifier) deriving Show
 newtype ResolvedValue = ResolvedValue (Value ResolvedValue) deriving Show

316f21dc1c10b3983eb877c29fd5abc013c66fd6
trampolinecall <>
2023-02-05 10:39:33 -0500

    somewhat implement name resolution

--- src/ir/UHF/IR/Decl.hs
+++ src/ir/UHF/IR/Decl.hs
@@ -6,6 +6,6 @@ import qualified UHF.IR.Value as Value
 
 import qualified Data.Map as Map
 
-data Decl identifier = Decl'Module (Module identifier) deriving Show
+data Decl value_identifier = Decl'Module (Module value_identifier) deriving Show
 
-data Module identifier = Module (Map.Map Text (Decl identifier)) (Map.Map Text (Value.Value identifier)) deriving Show
+data Module value_identifier = Module (Map.Map Text (Decl value_identifier)) (Map.Map Text (Value.Value value_identifier)) deriving Show

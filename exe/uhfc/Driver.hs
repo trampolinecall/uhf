7cde28796cccdb23a2e2e077f5b83f54ccaba5e4
Jason N <fireworkinyourbathtub@gmail.com>
2023-02-17 21:14:13 -0500

    add Compiler monad

--- exe/uhfc/Driver.hs
+++ exe/uhfc/Driver.hs
@@ -10,6 +10,8 @@ import qualified UHF.IO.File as File
 import qualified UHF.IO.Location as Location
 import qualified UHF.Diagnostic as Diagnostic
 
+import qualified UHF.Compiler as Compiler
+
 import qualified UHF.Token as Token
 import qualified UHF.AST as AST
 import qualified UHF.IR as IR
@@ -21,23 +23,14 @@ import qualified UHF.NameResolve as NameResolve
 import qualified UHF.InfixGroup as InfixGroup
 import qualified UHF.Type as Type
 
-type ErrorAccumulated a = Writer [Diagnostic.Error] a -- TODO: allow for warnings too
-
 type Tokens = ([Token.LToken], Token.LToken)
 type AST = [AST.Decl]
 type FirstIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (IR.TypeExpr (IR.NameContext, [Location.Located Text]))) IR.NominalTypeKey, Arena.Arena (IR.Binding (IR.NameContext, [Location.Located Text]) (IR.TypeExpr (IR.NameContext, [Location.Located Text])) () ()) IR.BindingKey, Arena.Arena (IR.BoundName ()) IR.BoundNameKey)
 type NRIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (IR.TypeExpr (Maybe IR.DeclKey))) IR.NominalTypeKey, Arena.Arena (IR.Binding (Maybe IR.BoundNameKey) (IR.TypeExpr (Maybe IR.DeclKey)) () ()) IR.BindingKey, Arena.Arena (IR.BoundName ()) IR.BoundNameKey)
 type TypedIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (Maybe (IR.Type Void))) IR.NominalTypeKey, Arena.Arena (IR.Binding (Maybe IR.BoundNameKey) (Maybe (IR.Type Void)) (Maybe (IR.Type Void)) Void) IR.BindingKey, Arena.Arena (IR.BoundName (Maybe (IR.Type Void))) IR.BoundNameKey)
 
-compile :: File.File -> Either [Diagnostic.Error] TypedIR
+compile :: File.File -> Compiler.Compiler TypedIR
 compile file =
-    let (res, diags) = runWriter $ compile' file
-    in if null diags
-        then Right res
-        else Left diags
-
-compile' :: File.File -> ErrorAccumulated TypedIR
-compile' file =
     convert_errors (Lexer.lex file) >>= \ (tokens, eof_tok) ->
     let (bt_error, ast) = Parser.parse tokens eof_tok
     in (case bt_error of
@@ -48,5 +41,7 @@ compile' file =
     let bindings' = InfixGroup.group bindings
     in convert_errors (Type.typecheck (decls, nominal_types, bindings', bound_names))
 
-convert_errors :: Diagnostic.IsError e => Writer [e] a -> Writer [Diagnostic.Error] a
-convert_errors = mapWriter (\ (res, errs) -> (res, map Diagnostic.to_error errs))
+-- convert_errors :: Diagnostic.IsError e => Writer [e] a -> Writer [Diagnostic.Error] a
+-- convert_errors = mapWriter (\ (res, errs) -> (res, map Diagnostic.to_error errs))
+
+convert_errors = identity

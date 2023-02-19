60472cf8480b61e2a1ae0cddce57afc14a404137
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-02-19 10:04:37 -0500

    move param nodes into a separate arena

--- exe/uhfc/Driver.hs
+++ exe/uhfc/Driver.hs
@@ -29,7 +29,7 @@ type AST = [AST.Decl]
 type FirstIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (IR.TypeExpr (IR.NameContext, [Location.Located Text]))) IR.NominalTypeKey, Arena.Arena (IR.Binding (IR.NameContext, [Location.Located Text]) (IR.TypeExpr (IR.NameContext, [Location.Located Text])) () ()) IR.BindingKey, Arena.Arena (IR.BoundValue ()) IR.BoundValueKey)
 type NRIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (IR.TypeExpr (Maybe IR.DeclKey))) IR.NominalTypeKey, Arena.Arena (IR.Binding (Located (Maybe IR.BoundValueKey)) (IR.TypeExpr (Maybe IR.DeclKey)) () ()) IR.BindingKey, Arena.Arena (IR.BoundValue ()) IR.BoundValueKey)
 type TypedIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (Maybe (IR.Type Void))) IR.NominalTypeKey, Arena.Arena (IR.Binding (Located (Maybe IR.BoundValueKey)) (Maybe (IR.Type Void)) (Maybe (IR.Type Void)) Void) IR.BindingKey, Arena.Arena (IR.BoundValue (Maybe (IR.Type Void))) IR.BoundValueKey)
-type GraphIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (Maybe (IR.Type Void))) IR.NominalTypeKey, Arena.Arena IR.GraphNode IR.GraphNodeKey, Arena.Arena (IR.BoundValue (Maybe (IR.Type Void))) IR.BoundValueKey)
+type GraphIR = (Arena.Arena IR.Decl IR.DeclKey, Arena.Arena (IR.NominalType (Maybe (IR.Type Void))) IR.NominalTypeKey, Arena.Arena IR.GraphNode IR.GraphNodeKey, Arena.Arena IR.GraphParam IR.GraphParamKey, Arena.Arena (IR.BoundValue (Maybe (IR.Type Void))) IR.BoundValueKey)
 
 compile :: File -> Either [Diagnostic.Error] GraphIR
 compile file =
@@ -49,8 +49,8 @@ compile' file =
     convert_errors (NameResolve.resolve (decls, nominal_types, bindings)) >>= \ (decls, nominal_types, bindings) ->
     let bindings' = InfixGroup.group bindings
     in convert_errors (Type.typecheck (decls, nominal_types, bindings', bound_values)) >>= \ (decls, nominal_types, bindings, bound_values) ->
-    let graph = (ToGraph.to_graph bound_values bindings)
-    in pure (decls, nominal_types, graph, bound_values)
+    let (nodes, params) = (ToGraph.to_graph bound_values bindings)
+    in pure (decls, nominal_types, nodes, params, bound_values)
 
 convert_errors :: Diagnostic.IsError e => Writer [e] a -> Writer [Diagnostic.Error] a
 convert_errors = mapWriter (\ (res, errs) -> (res, map Diagnostic.to_error errs))

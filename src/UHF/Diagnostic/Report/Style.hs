3403110bd67fc97b4b19ae0c094bbd8b06373dc5
Jason N <fireworkinyourbathtub@gmail.com>
2023-02-20 09:07:06 -0500

    add unicode style

--- src/UHF/Diagnostic/Report/Style.hs
+++ src/UHF/Diagnostic/Report/Style.hs
@@ -1,15 +1,18 @@
 module UHF.Diagnostic.Report.Style
     ( Style(..)
     , default_style
+    , unicode_style
     ) where
 
 import UHF.Util.Prelude
+
+import qualified UHF.IO.FormattedString as FormattedString
 
 import System.Console.ANSI
 
 data Style
     = Style
-        { file_path_color, error_color, warning_color, debug_message_color, note_color, hint_color, code_color, desc_color :: [SGR]
+        { file_path_color, error_color, warning_color, debug_message_color, note_color, hint_color :: [SGR]
         , und_other_color :: [SGR]
 
         , msg_error_char, msg_warning_char, msg_note_char, msg_hint_char, msg_error_char_top, msg_warning_char_top, msg_note_char_top, msg_hint_char_top :: Char
@@ -17,6 +20,8 @@ data Style
 
         , message_prefix_line, message_prefix_last :: Char
         , message_prefix :: Text
+
+        , make_footer :: Text -> Text -> FormattedString.FormattedString
         }
 
 default_style :: Style
@@ -27,8 +32,6 @@ default_style = Style
     , debug_message_color = [bold]
     , note_color = [bold, fg_bgreen]
     , hint_color = [bold, fg_bblue]
-    , code_color = [bold]
-    , desc_color = [bold]
 
     , und_other_color = [bold]
 
@@ -47,6 +50,38 @@ default_style = Style
     , message_prefix_line = '|'
     , message_prefix_last = '`'
     , message_prefix = "--"
+
+    , make_footer = \ code desc -> "==> [" <> FormattedString.color_text [bold] code <> "] " <> FormattedString.color_text [bold] desc
+    }
+unicode_style :: Style
+unicode_style = Style -- TODO
+    { file_path_color = [bold, fg_bcyan]
+    , error_color = [bold, fg_bred]
+    , warning_color = [bold, fg_bmagenta]
+    , debug_message_color = [bold]
+    , note_color = [bold, fg_bgreen]
+    , hint_color = [bold, fg_bblue]
+
+    , und_other_color = [bold]
+
+    -- TODO: connect underlines to messages
+    , msg_error_char = '━'
+    , msg_warning_char = '━'
+    , msg_note_char = '─'
+    , msg_hint_char = '─'
+    , msg_error_char_top = '━'
+    , msg_warning_char_top = '━'
+    , msg_note_char_top = '─'
+    , msg_hint_char_top = '─'
+    , file_line_char = '╭'
+    , header_line_char = '╞'
+    , other_line_char = '│'
+
+    , message_prefix_line = '├'
+    , message_prefix_last = '╰'
+    , message_prefix = "──"
+
+    , make_footer = \ code desc -> "═╧══[" <> FormattedString.color_text [bold] code <> "] " <> FormattedString.color_text [bold] desc
     }
 
 bold, bg_white, bg_cyan, bg_magenta, bg_blue, bg_yellow, bg_green, bg_red, bg_black, bg_bwhite, bg_bcyan, bg_bmagenta, bg_bblue, bg_byellow, bg_bgreen, bg_bred, bg_bblack, fg_white, fg_cyan, fg_magenta, fg_blue, fg_yellow, fg_green, fg_red, fg_black, fg_bwhite, fg_bcyan, fg_bmagenta, fg_bblue, fg_byellow, fg_bgreen, fg_bred, fg_bblack, reset, normal :: SGR

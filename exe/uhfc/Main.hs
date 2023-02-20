2f74bdce0105c1001c680af8297b1772b86cef02
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-02-19 20:08:22 -0500

    put diagnostic settings in flags

--- exe/uhfc/Main.hs
+++ exe/uhfc/Main.hs
@@ -11,33 +11,48 @@ import qualified UHF.Diagnostic.Settings as DiagnosticSettings
 
 import qualified UHF.IO.File as File
 
-newtype Args = Args [FilePath]
+data Args = Args [FilePath] DiagnosticSettings.Settings
 
 argparser :: ParserInfo Args
-argparser = info (args <**> helper)
-    ( fullDesc
-    <> progDesc "compile one or more uhf files"
-    <> header "uhfc: uhf compiler"
-    )
+argparser =
+    info
+        (args <**> helper)
+        ( fullDesc
+            <> header "uhfc: uhf compiler"
+        )
     where
-        args = Args <$>
-            some (
-                argument str
-                    ( metavar "FILES..."
-                    <> help "files to compile"
-                    )
-            )
+        args = Args
+            <$> some (
+                    argument str
+                        ( metavar "FILES..."
+                        <> help "files to compile"
+                        )
+                )
+            <*> (DiagnosticSettings.Settings
+                    <$> option
+                        (eitherReader $ \case
+                            "always" -> Right DiagnosticSettings.Colors
+                            "never" -> Right DiagnosticSettings.NoColors
+                            "auto" -> Right DiagnosticSettings.AutoDetect
+                            _ -> Left "invalid option: must be one of 'always', 'never', or 'auto'" -- TODO: figure out how to do this better
+                        )
+                        (long "colors"
+                            <> metavar "COLORS"
+                            <> value (DiagnosticSettings.AutoDetect)
+                            <> help "when to print colors in diagnostics"
+                        )
+                )
 
 main :: IO ()
 main =
-    execParser argparser >>= \ (Args files) ->
+    execParser argparser >>= \ (Args files diagnostic_settings) ->
     let total_files = length files
-    in mapM_ (\ (num, f) -> compile num total_files f) (zip [1..] files)
+    in mapM_ (\ (num, f) -> compile diagnostic_settings num total_files f) (zip [1..] files)
 
-compile :: Int -> Int -> FilePath -> IO ()
-compile num total fname =
+compile :: DiagnosticSettings.Settings -> Int -> Int -> FilePath -> IO ()
+compile diagnostic_settings num total fname =
     File.open_file fname >>= \ f ->
     putStrLn ("[" <> show num <> "/" <> show total <> "]: compiling " <> format f) >>
-    Compiler.run_compiler (Driver.compile f) (DiagnosticSettings.Settings DiagnosticSettings.AutoDetect) >>= \case
+    Compiler.run_compiler (Driver.compile f) diagnostic_settings >>= \case
         Just res -> putTextLn (show res)
         Nothing -> pure ()

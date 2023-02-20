6661a24b66a54b97b3d90f4103ab0622b6055dc8
trampolinecall <>
2023-02-20 10:07:07 -0500

    move Driver to library

--- exe/uhfc/Main.hs
+++ exe/uhfc/Main.hs
@@ -6,11 +6,7 @@ import UHF.Util.Prelude
 
 import Options.Applicative
 
-import qualified Driver
-
-import qualified UHF.IO.File as File
-
-import qualified UHF.Compiler as Compiler
+import qualified UHF.Driver as Driver
 
 import qualified UHF.IO.FormattedString as FormattedString
 import qualified UHF.Diagnostic.Settings as DiagnosticSettings
@@ -50,13 +46,7 @@ argparser =
 main :: IO ()
 main =
     execParser argparser >>= \ (Args files c_needed diagnostic_settings) ->
-    let total_files = length files
-    in mapM_ (\ (num, f) -> compile c_needed diagnostic_settings num total_files f) (zip [1..] files)
-
-compile :: FormattedString.ColorsNeeded -> DiagnosticSettings.Settings -> Int -> Int -> FilePath -> IO ()
-compile c_needed diagnostic_settings num total fname =
-    File.open fname >>= \ f -> -- TODO: put this in driver
-    Compiler.run_compiler (Driver.compile f) c_needed diagnostic_settings >>= \case
-        Just (Just res) -> putTextLn res -- TODO: get rid of double Maybe
-        Just Nothing -> exitFailure -- TODO: decide what should happen here
-        Nothing -> exitFailure
+    mapM (Driver.compile c_needed diagnostic_settings) files >>= \ results ->
+    if any (isLeft) results
+       then exitFailure
+       else pure ()

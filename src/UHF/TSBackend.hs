84691fb05f5ccd86e08b91c23e2deede3809e7de
trampolinecall <>
2023-02-25 10:23:35 -0500

    make make thunk graph functions return all the nodes they created

--- src/UHF/TSBackend.hs
+++ src/UHF/TSBackend.hs
@@ -48,7 +48,8 @@ runtime_code = $(FileEmbed.embedStringFile "data/ts_runtime.ts")
 data TSDecl
 data TSNominalType = TSNominalType'Data HIR.NominalTypeKey Text -- TODO: actually implement variants and things
 data TSLambda = TSLambda ANFIR.NodeKey Type Type ANFIR.NodeKey -- TODO: captures
-data TSMakeThunkGraph = TSMakeThunkGraph ANFIR.NodeKey (Set ANFIR.NodeKey) (Set ANFIR.ParamKey)
+data MakeThunkGraphFor = LambdaBody ANFIR.NodeKey | Globals
+data TSMakeThunkGraph = TSMakeThunkGraph MakeThunkGraphFor (Set ANFIR.NodeKey) (Set ANFIR.ParamKey)
 data TS = TS [TSDecl] [TSNominalType] [TSMakeThunkGraph] [TSLambda]
 
 instance Semigroup TS where
@@ -76,8 +77,7 @@ stringify_ts_nominal_type (TSNominalType'Data key name) =
             <> "}\n"
 
 stringify_ts_make_thunk_graph :: TSMakeThunkGraph -> IRReader Text
-stringify_ts_make_thunk_graph (TSMakeThunkGraph main_key included_nodes included_params) =
-    node_type main_key >>= refer_type >>= \ main_node_type ->
+stringify_ts_make_thunk_graph (TSMakeThunkGraph for included_nodes included_params) =
     mapM stringify_param (Set.toList included_params) >>= \ params_stringified ->
 
     concat <$> mapM stringify_node_decl (Set.toList included_nodes) >>= \ node_decls ->
@@ -85,12 +85,29 @@ stringify_ts_make_thunk_graph (TSMakeThunkGraph main_key included_nodes included
     -- TODO: dont use GraphParamKey Ord instance to decide parameter order (not deterministic across compiles)
     -- also TODO: dont use unmake_key anywhere (probably including outside of this module too)
 
-    pure ("function " <> mangle_graph_node_as_make_thunk_graph main_key <> "(" <> Text.intercalate ", " params_stringified <> "): " <> main_node_type <> " {\n" -- TODO: captures
-        <> Text.unlines node_decls
-        <> Text.unlines node_set_fields
-        <> "    return " <> mangle_graph_node_as_local_thunk main_key <> ";\n"
+    ts_return_type >>= \ ts_return_type ->
+
+    pure ("function " <> mangle_make_thunk_graph_for for <> "(" <> Text.intercalate ", " params_stringified <> "): " <> ts_return_type <> " {\n" -- TODO: captures
+        <> Text.unlines (map ("    " <>) node_decls)
+        <> "\n"
+        <> Text.unlines (map ("    " <>) node_set_fields)
+        <> "\n"
+        <> "    return " <> object_of_nodes <> ";\n"
         <> "}\n")
     where
+        ts_return_type =
+            mapM r (Set.toList included_nodes) >>= \ tys ->
+            pure ("{ " <> Text.intercalate "; " tys <> " }")
+            where
+                r node =
+                    node_type node >>= refer_type >>= \ ty ->
+                    pure (mangle_graph_node_as_node_var node <> ": " <> ty)
+
+        object_of_nodes = "{ " <> Text.intercalate ", " (map r (Set.toList included_nodes)) <> " }"
+            where
+                r node =
+                    mangle_graph_node_as_node_var node <> ": " <> mangle_graph_node_as_local_thunk node
+
         mangle_graph_node_as_local_thunk, mangle_graph_node_as_local_evaluator :: ANFIR.NodeKey -> Text
         mangle_graph_node_as_local_thunk key = "thunk" <> show (Arena.unmake_key key)
         mangle_graph_node_as_local_evaluator key = "evaluator" <> show (Arena.unmake_key key)
@@ -114,12 +131,12 @@ stringify_ts_make_thunk_graph (TSMakeThunkGraph main_key included_nodes included
                 ANFIR.Node'Bool _ b -> pure [let_evaluator "ConstEvaluator<bool>" (evaluator "ConstEvaluator" (if b then "true" else "false")), default_let_thunk]
                 ANFIR.Node'Char _ c -> pure [let_evaluator "ConstEvaluator<char>" (evaluator "ConstEvaluator" (show c)), default_let_thunk]
                 ANFIR.Node'String _ s -> pure [let_evaluator "ConstEvaluator<string>" (evaluator "ConstEvaluator" (show s)), default_let_thunk]
-                ANFIR.Node'Tuple ty a b ->
+                ANFIR.Node'Tuple _ a b ->
                         node_type a >>= refer_type_raw >>= \ a_ty ->
                         node_type b >>= refer_type_raw >>= \ b_ty ->
                         pure [let_evaluator ("TupleEvaluator<" <> a_ty <> ", " <> b_ty <> ">") (evaluator "TupleEvaluator" "undefined, undefined"), default_let_thunk]
 
-                ANFIR.Node'Lambda _ _ _ -> pure [let_evaluator ("ConstEvaluator<" <> mangle_graph_node_as_lambda node_key <> ">") (evaluator "ConstEvaluator" ("new " <> mangle_graph_node_as_lambda node_key)), default_let_thunk] -- TODO: put captures here
+                ANFIR.Node'Lambda _ _ _ -> pure [let_evaluator ("ConstEvaluator<" <> mangle_graph_node_as_lambda node_key <> ">") (evaluator "ConstEvaluator" ("new " <> mangle_graph_node_as_lambda node_key <> "()")), default_let_thunk] -- TODO: put captures here
                 ANFIR.Node'Param _ param_key -> pure [let_thunk $ "param_" <> show (Arena.unmake_key param_key)]
 
                 ANFIR.Node'Call ty _ arg ->
@@ -158,7 +175,7 @@ stringify_ts_lambda (TSLambda key arg result body_key) =
     refer_type result >>= \ result_type ->
     pure ("class " <> mangle_graph_node_as_lambda key <> " implements Lambda<" <> arg_type <> ", " <> result_type <> "> {\n"
         <> "    call(arg: " <> arg_type <> "): " <> result_type <> " {\n"
-        <> "        return " <> mangle_graph_node_as_make_thunk_graph body_key <> "();\n" -- TODO: captures
+        <> "        return " <> mangle_make_thunk_graph_for (LambdaBody key) <> "()." <> mangle_graph_node_as_node_var body_key <> ";\n" -- TODO: captures and params
         <> "    }\n"
         <> "}\n")
 
@@ -190,6 +207,7 @@ lower decls nominal_types nodes params =
                 Arena.transform_with_keyM define_decl decls >>
                 Arena.transform_with_keyM define_nominal_type nominal_types >>
                 Arena.transform_with_keyM define_lambda_type nodes >>
+                -- TODO: probably move collect involved nodes outside of define_lambda_type, also have to store global nodes in main module, tell_make_thunk_graph (TSMakeThunkGraph Globals global_nodes Set.empty) -- global thunk graph does not have any params
                 pure ()
             ) >>= \ ((), TS ts_decls ts_nominal_types ts_make_thunk_graphs ts_lambdas) ->
 
@@ -215,7 +233,7 @@ define_lambda_type key (ANFIR.Node'Lambda _ param body) = -- TODO: annotate with
     lift (get_param param) >>= \ (ANFIR.Param param_ty) ->
     lift (node_type body) >>= \ body_type ->
     lift (get_included_nodes body) >>= \ (included_nodes, included_params) ->
-    tell_make_thunk_graph (TSMakeThunkGraph body included_nodes included_params) >>
+    tell_make_thunk_graph (TSMakeThunkGraph (LambdaBody key) included_nodes included_params) >>
     tell_lambda (TSLambda key param_ty body_type body)
     where
         get_included_nodes :: ANFIR.NodeKey -> IRReader (Set ANFIR.NodeKey, Set ANFIR.ParamKey)
@@ -254,5 +272,9 @@ mangle_nominal_type key = "NominalType" <> show (Arena.unmake_key key)
 mangle_graph_node_as_lambda :: ANFIR.NodeKey -> Text
 mangle_graph_node_as_lambda key = "Lambda" <> show (Arena.unmake_key key)
 
-mangle_graph_node_as_make_thunk_graph :: ANFIR.NodeKey -> Text
-mangle_graph_node_as_make_thunk_graph key = "make_thunk_graph" <> show (Arena.unmake_key key)
+mangle_graph_node_as_node_var :: ANFIR.NodeKey -> Text
+mangle_graph_node_as_node_var key = "node_" <> show (Arena.unmake_key key)
+
+mangle_make_thunk_graph_for :: MakeThunkGraphFor -> Text
+mangle_make_thunk_graph_for Globals = "make_global_thunk_graph"
+mangle_make_thunk_graph_for (LambdaBody lambda_key) = "make_thunk_graph_for_" <> mangle_graph_node_as_lambda lambda_key -- TODO: put this in the lambda itself?

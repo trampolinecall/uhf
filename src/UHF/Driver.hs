3e02001ee826a7e6da06d1e6e2b9409dd53ccd14
trampolinecall <>
2023-02-25 14:34:23 -0500

    implement arguments for output file type and write them to files

--- src/UHF/Driver.hs
+++ src/UHF/Driver.hs
@@ -1,8 +1,14 @@
 module UHF.Driver
-    ( compile
+    ( CompileOptions(..)
+    , OutputFormat(..)
+    , compile
     ) where
 
 import UHF.Util.Prelude
+
+import qualified Data.Text.IO as Text.IO
+
+import qualified System.FilePath as FilePath
 
 import qualified Arena
 
@@ -37,37 +43,65 @@ type AST = [AST.Decl]
 type FirstIR = (Arena.Arena (HIR.Decl (HIR.NameContext, [Located Text]) (HIR.TypeExpr (HIR.NameContext, [Located Text])) () ()) HIR.DeclKey, Arena.Arena (HIR.NominalType (HIR.TypeExpr (HIR.NameContext, [Located Text]))) HIR.NominalTypeKey, Arena.Arena (HIR.BoundValue ()) HIR.BoundValueKey)
 type NRIR = (Arena.Arena (HIR.Decl (Located (Maybe HIR.BoundValueKey)) (HIR.TypeExpr (Maybe HIR.DeclKey)) () ()) HIR.DeclKey, Arena.Arena (HIR.NominalType (HIR.TypeExpr (Maybe HIR.DeclKey))) HIR.NominalTypeKey, Arena.Arena (HIR.BoundValue ()) HIR.BoundValueKey)
 type TypedIR = (Arena.Arena (HIR.Decl (Located (Maybe HIR.BoundValueKey)) (Maybe (HIR.Type Void)) (Maybe (HIR.Type Void)) Void) HIR.DeclKey, Arena.Arena (HIR.NominalType (Maybe (HIR.Type Void))) HIR.NominalTypeKey, Arena.Arena (HIR.BoundValue (Maybe (HIR.Type Void))) HIR.BoundValueKey)
-type GraphIR = (Arena.Arena (HIR.Decl (Located (Maybe HIR.BoundValueKey)) (Maybe (HIR.Type Void)) (Maybe (HIR.Type Void)) Void) HIR.DeclKey, Arena.Arena (HIR.NominalType (Maybe (HIR.Type Void))) HIR.NominalTypeKey, Arena.Arena (ANFIR.Node (Maybe (HIR.Type Void)) ()) ANFIR.NodeKey, Arena.Arena (ANFIR.Param (Maybe (HIR.Type Void))) ANFIR.ParamKey, Arena.Arena (HIR.BoundValue (Maybe (HIR.Type Void))) HIR.BoundValueKey)
-type NoPoisonIR = (Arena.Arena (HIR.Decl (Located (Maybe HIR.BoundValueKey)) (Maybe (HIR.Type Void)) (Maybe (HIR.Type Void)) Void) HIR.DeclKey, Arena.Arena (HIR.NominalType (HIR.Type Void)) HIR.NominalTypeKey, Arena.Arena (ANFIR.Node (HIR.Type Void) Void) ANFIR.NodeKey, Arena.Arena (ANFIR.Param (HIR.Type Void)) ANFIR.ParamKey, Arena.Arena (HIR.BoundValue (HIR.Type Void)) HIR.BoundValueKey)
+type GraphIR = (Arena.Arena ANFIR.Decl HIR.DeclKey, Arena.Arena (HIR.NominalType (Maybe (HIR.Type Void))) HIR.NominalTypeKey, Arena.Arena (ANFIR.Node (Maybe (HIR.Type Void)) ()) ANFIR.NodeKey, Arena.Arena (ANFIR.Param (Maybe (HIR.Type Void))) ANFIR.ParamKey, Arena.Arena (HIR.BoundValue (Maybe (HIR.Type Void))) HIR.BoundValueKey)
+type NoPoisonIR = (Arena.Arena ANFIR.Decl HIR.DeclKey, Arena.Arena (HIR.NominalType (HIR.Type Void)) HIR.NominalTypeKey, Arena.Arena (ANFIR.Node (HIR.Type Void) Void) ANFIR.NodeKey, Arena.Arena (ANFIR.Param (HIR.Type Void)) ANFIR.ParamKey, Arena.Arena (HIR.BoundValue (HIR.Type Void)) HIR.BoundValueKey)
 type Dot = Text
 type TS = Text
 
-compile :: FormattedString.ColorsNeeded -> DiagnosticSettings.Settings -> FilePath -> IO (Either () ())
-compile c_needed diagnostic_settings path =
-    File.open path >>= \ file ->
-    Compiler.run_compiler (compile' file) c_needed diagnostic_settings >>= \case
-        Just (Just res) -> putTextLn res >> pure (Right ()) -- TODO: get rid of double Maybe
-        Just Nothing -> pure (Left ()) -- TODO: decide what should happen here
+data OutputFormat = AST | Dot | TS deriving Eq
+data CompileOptions
+    = CompileOptions
+        { input_file :: FilePath
+        , module_name :: Maybe Text
+        , output_formats :: [OutputFormat]
+        }
+
+compile :: FormattedString.ColorsNeeded -> DiagnosticSettings.Settings -> CompileOptions -> IO (Either () ())
+compile c_needed diagnostic_settings compile_options =
+    File.open (input_file compile_options) >>= \ file ->
+    Compiler.run_compiler (front file >>= \ ast -> analysis ast >>= \ ir -> pure (ast, ir)) c_needed diagnostic_settings >>= \case
+        Just (ast, Just ir) ->
+            (if AST `elem` output_formats compile_options -- TODO: should outputting ast skip anaylsis?
+                then todo
+                else pure ()) >>
+            (if Dot `elem` output_formats compile_options
+                then write_output_file "dot" (to_dot ir)
+                else pure ()) >>
+            (if TS `elem` output_formats compile_options
+                then write_output_file "ts" (to_ts ir)
+                else pure ()) >>
+            pure (Right ())
+
+        Just (_, Nothing) -> pure (Left ())
         Nothing -> pure (Left ())
 
-compile' :: File -> Compiler.Compiler (Maybe TS)
-compile' file =
-    -- TODO: clean up
+    where
+        module_name' =
+            case module_name compile_options of
+                Just m -> convert_str m :: FilePath
+                Nothing -> FilePath.takeBaseName (input_file compile_options)
+
+        get_output_file_path ext = FilePath.takeDirectory (input_file compile_options) FilePath.</> module_name' FilePath.<.> ext
+
+        write_output_file ext contents = Text.IO.writeFile (get_output_file_path ext) contents
+
+front :: File -> Compiler.Compiler AST
+front file =
     Lexer.lex file >>= \ (tokens, eof_tok) ->
-    Parser.parse tokens eof_tok >>= \ ast ->
+    Parser.parse tokens eof_tok
+
+analysis :: AST -> Compiler.Compiler (Maybe NoPoisonIR)
+analysis ast =
     ASTToIR.convert ast >>= \ (decls, nominal_types, bound_values) ->
     NameResolve.resolve (decls, nominal_types) >>= \ (decls, nominal_types) ->
     let decls' = InfixGroup.group decls
     in Type.typecheck (decls', nominal_types, bound_values) >>= \ (decls', nominal_types, bound_values) ->
     let (decls'', nodes, params) = ToGraph.to_graph bound_values decls'
         no_poison = RemovePoison.remove_poison (decls'', nominal_types, nodes, params, bound_values)
-        dot =
-            case no_poison of
-                Just (decls'', nominal_types', nodes', params', bound_values') -> Just $ ToDot.to_dot decls'' nominal_types' nodes' params'
-                Nothing -> Nothing
+    in pure no_poison
 
-        ts =
-            case no_poison of
-                Just (decls'', nominal_types', nodes', params', bound_values') -> Just $ TSBackend.lower decls'' nominal_types' nodes' params'
-                Nothing -> Nothing
-    in pure ts
+to_ts :: NoPoisonIR -> TS
+to_ts (decls, nominal_types, nodes, params, bound_values) = TSBackend.lower decls nominal_types nodes params
+
+to_dot :: NoPoisonIR -> Dot
+to_dot (decls, nominal_types, nodes, params, bound_values) = ToDot.to_dot decls nominal_types nodes params

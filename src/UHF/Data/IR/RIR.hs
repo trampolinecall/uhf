460b65bfb7328d326cc53cf72466c6bf369ae16d
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-02-26 09:53:21 -0500

    add ToRIR phase

--- src/UHF/Data/IR/RIR.hs
+++ src/UHF/Data/IR/RIR.hs
@@ -28,37 +28,37 @@ data Binding = Binding BoundValueKey Expr deriving Show
 type Type = Type.Type Void
 
 data Expr
-    = Expr'Identifier Type Span BoundValueKey
-    | Expr'Char Type Span Char
-    | Expr'String Type Span Text
-    | Expr'Int Type Span Integer
-    | Expr'Float Type Span Rational
-    | Expr'Bool Type Span Bool -- TODO: replace with identifier exprs
+    = Expr'Identifier (Maybe Type) Span (Maybe BoundValueKey)
+    | Expr'Char (Maybe Type) Span Char
+    | Expr'String (Maybe Type) Span Text
+    | Expr'Int (Maybe Type) Span Integer
+    | Expr'Float (Maybe Type) Span Rational
+    | Expr'Bool (Maybe Type) Span Bool -- TODO: replace with identifier exprs
 
-    | Expr'Tuple Type Span Expr Expr
+    | Expr'Tuple (Maybe Type) Span Expr Expr
 
-    | Expr'Lambda Type Span Pattern Expr
+    | Expr'Lambda (Maybe Type) Span Pattern Expr
 
-    | Expr'Let Type Span [Binding] Expr
+    | Expr'Let (Maybe Type) Span [Binding] Expr
 
-    | Expr'Call Type Span Expr Expr
+    | Expr'Call (Maybe Type) Span Expr Expr
 
-    | Expr'If Type Span Span Expr Expr Expr -- TODO: desugar to 'case x { true -> _; false -> _ }
-    | Expr'Case Type Span Span Expr [(Pattern, Expr)] -- TODO: case should be more like switch at this level
+    | Expr'If (Maybe Type) Span Expr Expr Expr -- TODO: desugar to 'case x { true -> _; false -> _ }
+    | Expr'Case (Maybe Type) Span Expr [(Pattern, Expr)] -- TODO: case should be more like switch at this level
 
-    | Expr'Poison Type Span
+    | Expr'Poison (Maybe Type) Span
     deriving Show
 
 data Pattern
-    = Pattern'Identifier Type Span BoundValueKey
-    | Pattern'Wildcard Type Span
-    | Pattern'Tuple Type Span Pattern Pattern
-    | Pattern'Named Type Span Span (Located BoundValueKey) Pattern
+    = Pattern'Identifier (Maybe Type) Span BoundValueKey
+    | Pattern'Wildcard (Maybe Type) Span
+    | Pattern'Tuple (Maybe Type) Span Pattern Pattern
+    | Pattern'Named (Maybe Type) Span (Located BoundValueKey) Pattern
 
-    | Pattern'Poison Type Span
+    | Pattern'Poison (Maybe Type) Span
     deriving Show
 
-expr_type :: Expr -> Type
+expr_type :: Expr -> (Maybe Type)
 expr_type (Expr'Identifier ty _ _) = ty
 expr_type (Expr'Char ty _ _) = ty
 expr_type (Expr'String ty _ _) = ty
@@ -69,8 +69,8 @@ expr_type (Expr'Tuple ty _ _ _) = ty
 expr_type (Expr'Lambda ty _ _ _) = ty
 expr_type (Expr'Let ty _ _ _) = ty
 expr_type (Expr'Call ty _ _ _) = ty
-expr_type (Expr'If ty _ _ _ _ _) = ty
-expr_type (Expr'Case ty _ _ _ _) = ty
+expr_type (Expr'If ty _ _ _ _) = ty
+expr_type (Expr'Case ty _ _ _) = ty
 expr_type (Expr'Poison ty _) = ty
 
 expr_span :: Expr -> Span
@@ -84,20 +84,20 @@ expr_span (Expr'Tuple _ sp _ _) = sp
 expr_span (Expr'Lambda _ sp _ _) = sp
 expr_span (Expr'Let _ sp _ _) = sp
 expr_span (Expr'Call _ sp _ _) = sp
-expr_span (Expr'If _ sp _ _ _ _) = sp
-expr_span (Expr'Case _ sp _ _ _) = sp
+expr_span (Expr'If _ sp _ _ _) = sp
+expr_span (Expr'Case _ sp _ _) = sp
 expr_span (Expr'Poison _ sp) = sp
 
-pattern_type :: Pattern -> Type
+pattern_type :: Pattern -> (Maybe Type)
 pattern_type (Pattern'Identifier ty _ _) = ty
 pattern_type (Pattern'Wildcard ty _) = ty
 pattern_type (Pattern'Tuple ty _ _ _) = ty
-pattern_type (Pattern'Named ty _ _ _ _) = ty
+pattern_type (Pattern'Named ty _ _ _) = ty
 pattern_type (Pattern'Poison ty _) = ty
 
 pattern_span :: Pattern -> Span
 pattern_span (Pattern'Identifier _ sp _) = sp
 pattern_span (Pattern'Wildcard _ sp) = sp
 pattern_span (Pattern'Tuple _ sp _ _) = sp
-pattern_span (Pattern'Named _ sp _ _ _) = sp
+pattern_span (Pattern'Named _ sp _ _) = sp
 pattern_span (Pattern'Poison _ sp) = sp

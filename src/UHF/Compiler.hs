3c25a97a6a2191e52c3f3e527182d0c428a63a53
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-02-26 16:43:29 -0500

    track diagnostics in Compiler using Seq instead of with lists

--- src/UHF/Compiler.hs
+++ src/UHF/Compiler.hs
@@ -12,12 +12,13 @@ import UHF.Util.Prelude hiding (error)
 
 import Control.Monad.Fix (MonadFix (mfix))
 
-import qualified UHF.Diagnostic as Diagnostic
-
 import qualified System.IO as IO
+
+import qualified Data.Sequence as Sequence
 
 import qualified UHF.IO.FormattedString as FormattedString
 
+import qualified UHF.Diagnostic as Diagnostic
 import qualified UHF.Diagnostic.Settings as DiagnosticSettings
 
 newtype Compiler r = Compiler { uncompiler :: WriterT Diagnostics IO r }
@@ -37,12 +38,12 @@ instance MonadFix Compiler where
 
 data Diagnostics
     = Diagnostics
-        { _errors :: [Diagnostic.Error]
-        , _warnings :: [Diagnostic.Warning]
+        { _errors :: Seq Diagnostic.Error
+        , _warnings :: Seq Diagnostic.Warning
         }
 
 instance Monoid Diagnostics where
-    mempty = Diagnostics [] []
+    mempty = Diagnostics Sequence.empty Sequence.empty
 instance Semigroup Diagnostics where
     Diagnostics e1 w1 <> Diagnostics e2 w2 = Diagnostics (e1 <> e2) (w1 <> w2)
 
@@ -56,10 +57,10 @@ run_compiler (Compiler r) c_needed diag_settings =
         else pure Nothing
 
 error :: Diagnostic.ToError e => e -> Compiler ()
-error e = Compiler $ tell (Diagnostics [Diagnostic.to_error e] [])
+error e = Compiler $ tell (Diagnostics (Sequence.singleton $ Diagnostic.to_error e) Sequence.empty)
 errors :: Diagnostic.ToError e => [e] -> Compiler ()
-errors es = Compiler $ tell (Diagnostics (map Diagnostic.to_error es) [])
+errors es = Compiler $ tell (Diagnostics (Sequence.fromList $ map Diagnostic.to_error es) Sequence.empty)
 warning :: Diagnostic.IsWarning w => w -> Compiler ()
-warning w = Compiler $ tell (Diagnostics [] [Diagnostic.to_warning w])
+warning w = Compiler $ tell (Diagnostics Sequence.empty (Sequence.singleton $ Diagnostic.to_warning w))
 warnings :: Diagnostic.IsWarning w => [w] -> Compiler ()
-warnings ws = Compiler $ tell (Diagnostics [] (map Diagnostic.to_warning ws))
+warnings ws = Compiler $ tell (Diagnostics Sequence.empty (Sequence.fromList $ map Diagnostic.to_warning ws))

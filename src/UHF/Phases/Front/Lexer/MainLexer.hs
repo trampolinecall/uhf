10a14bd516418fa2c754a836fd70522eae5dbd0f
Jason N <fireworkinyourbathtub@gmail.com>
2023-02-26 15:04:03 -0500

    remove LexLocation and put remaining in Location

--- src/UHF/Phases/Front/Lexer/MainLexer.hs
+++ src/UHF/Phases/Front/Lexer/MainLexer.hs
@@ -26,7 +26,7 @@ import Data.Char (isAlpha, isDigit, isOctDigit, isHexDigit, isSpace, digitToInt)
 lex :: File -> Writer [LexError.Error] (Sequence.Seq Token.LInternalToken, Token.LToken)
 lex f =
     let eof = Located (Span.end_of_file f) (Token.EOF ())
-    in evalStateT (run Sequence.Empty) (LexLocation (Location.new f) (File.contents f)) >>= \ toks -> pure (toks, eof)
+    in evalStateT (run Sequence.Empty) (Location.new f) >>= \ toks -> pure (toks, eof)
     where
         run toks =
             get >>= \ l ->
@@ -34,8 +34,7 @@ lex f =
                 then pure toks
                 else lex_one_token >>= \ more -> run (toks <> more)
 -- lex_one_token {{{2
-data LexLocation = LexLocation Location Text deriving Show
-lex_one_token :: StateT LexLocation (Writer [LexError.Error]) (Sequence.Seq Token.LInternalToken)
+lex_one_token :: StateT Location (Writer [LexError.Error]) (Sequence.Seq Token.LInternalToken)
 lex_one_token =
     StateT $ \ loc ->
         writer $
@@ -52,7 +51,7 @@ lex_one_token =
                 , make_bad_char
                 ]
 -- Lexer {{{2
-type Lexer = StateT LexLocation (WriterT [LexError.Error] Maybe)
+type Lexer = StateT Location (WriterT [LexError.Error] Maybe)
 -- lexing functions {{{2
 lex_comment :: Lexer (Sequence.Seq Token.LInternalToken)
 lex_comment =
@@ -210,15 +209,15 @@ lex_space = consume isSpace >> pure Sequence.Empty
 make_bad_char :: Lexer (Sequence.Seq Token.LInternalToken)
 make_bad_char = consume (const True) >>= \ (Located sp c) -> put_error (LexError.BadChar c sp) >> pure Sequence.Empty
 -- helper functions {{{1
-remaining :: LexLocation -> Text
-remaining (LexLocation _ r) = r
+remaining :: Location -> Text
+remaining = Location.loc_remaining_in_file
 
-at_end :: LexLocation -> Bool
-at_end (LexLocation _ r) = Text.null r
+at_end :: Location -> Bool
+at_end = Text.null . remaining
 
-new_span_start_and_end :: LexLocation -> LexLocation -> Span
+new_span_start_and_end :: Location -> Location -> Span
 -- start and end should be in the same file because the lex function never processes more than one file at a time
-new_span_start_and_end (LexLocation start _) (LexLocation end _) = Span.new start 0 (Location.loc_ind end - Location.loc_ind start)
+new_span_start_and_end start end = Span.new start 0 (Location.loc_ind end - Location.loc_ind start)
 
 choice :: [Lexer a] -> Lexer a
 choice [] = StateT $ \ _ -> WriterT Nothing
@@ -228,12 +227,12 @@ choice (fn:fns) = StateT $ \ loc -> WriterT $
         Just res -> Just res
 
 consume :: (Char -> Bool) -> Lexer (Located Char)
-consume p = StateT $ \ (LexLocation loc remaining) -> WriterT $
-    case Text.uncons remaining of
-        Just (c, more) | p c -> Just ((Located (Span.new loc 0 1) c, LexLocation (Location.seek 1 loc) more), [])
+consume p = StateT $ \ loc -> WriterT $
+    case Text.uncons $ remaining loc of
+        Just (c, _) | p c -> Just ((Located (Span.new loc 0 1) c, Location.seek_1 loc), [])
         _ -> Nothing
 
-get_loc :: Lexer LexLocation
+get_loc :: Lexer Location
 get_loc = get
 
 put_error :: LexError.Error -> Lexer ()
@@ -258,9 +257,9 @@ case_lex_empty =
         ((Sequence.Empty, _), []) -> pure ()
         x -> assertFailure $ "lex lexed incorrectly: returned '" ++ show x ++ "'"
 
-lex_test :: (LexLocation -> r) -> Text -> (r -> IO ()) -> IO ()
-lex_test fn input check = File.new "a" input >>= \ f -> check $ fn $ (LexLocation (Location.new f) input)
-lex_test' :: Lexer r -> Text -> (Maybe (LexLocation, [LexError.Error], r) -> IO ()) -> IO ()
+lex_test :: (Location -> r) -> Text -> (r -> IO ()) -> IO ()
+lex_test fn input check = File.new "a" input >>= \ f -> check $ fn $ (Location.new f)
+lex_test' :: Lexer r -> Text -> (Maybe (Location, [LexError.Error], r) -> IO ()) -> IO ()
 lex_test' fn = lex_test (((\ ((r, loc), errs) -> (loc, errs, r)) <$>) . runWriterT . runStateT fn)
 lex_test_fail :: Show r => [Char] -> r -> IO a
 lex_test_fail fn_name res = assertFailure $ "'" ++ fn_name ++ "' lexed incorrectly: returned '" ++ show res ++ "'"

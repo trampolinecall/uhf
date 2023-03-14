452d5edb4ab7dd27e06b990cd3f2d93737bb467f
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-03-14 12:15:10 -0400

    rename type parameters in hir and split up typing into many modules

--- src/UHF/Data/IR/HIR.hs
+++ src/UHF/Data/IR/HIR.hs
@@ -34,14 +34,14 @@ import UHF.IO.Located (Located)
 
 data HIR iden type_expr type_info binary_ops_allowed = HIR (Arena.Arena (Decl iden type_expr type_info binary_ops_allowed) DeclKey) (Arena.Arena (Type.ADT type_expr) ADTKey) (Arena.Arena (Type.TypeSynonym type_expr) TypeSynonymKey) (Arena.Arena (BoundValue type_info) BoundValueKey) DeclKey
 
-data Decl identifier typeannotation typeinfo binaryopsallowed
-    = Decl'Module NameContext [Binding identifier typeannotation typeinfo binaryopsallowed] [ADTKey] [TypeSynonymKey]
+data Decl identifier type_expr type_info binary_ops_allowed
+    = Decl'Module NameContext [Binding identifier type_expr type_info binary_ops_allowed] [ADTKey] [TypeSynonymKey]
     | Decl'Type (Type.Type Void)
     deriving Show
 
-data BoundValue typeinfo = BoundValue typeinfo Span deriving Show
+data BoundValue type_info = BoundValue type_info Span deriving Show
 
-data Binding identifier typeannotation typeinfo binaryopsallowed = Binding (Pattern identifier typeinfo) Span (Expr identifier typeannotation typeinfo binaryopsallowed) deriving Show
+data Binding identifier type_expr type_info binary_ops_allowed = Binding (Pattern identifier type_info) Span (Expr identifier type_expr type_info binary_ops_allowed) deriving Show
 
 data NameContext = NameContext (Map.Map Text DeclKey) (Map.Map Text BoundValueKey) (Maybe NameContext) deriving Show
 
@@ -51,59 +51,59 @@ data TypeExpr identifier
     | TypeExpr'Poison Span
     deriving Show
 
-data Expr identifier typeannotation typeinfo binaryopsallowed
-    = Expr'Identifier typeinfo Span identifier
-    | Expr'Char typeinfo Span Char
-    | Expr'String typeinfo Span Text
-    | Expr'Int typeinfo Span Integer
-    | Expr'Float typeinfo Span Rational
-    | Expr'Bool typeinfo Span Bool -- TODO: replace with identifier exprs
+data Expr identifier type_expr type_info binary_ops_allowed
+    = Expr'Identifier type_info Span identifier
+    | Expr'Char type_info Span Char
+    | Expr'String type_info Span Text
+    | Expr'Int type_info Span Integer
+    | Expr'Float type_info Span Rational
+    | Expr'Bool type_info Span Bool -- TODO: replace with identifier exprs
 
-    | Expr'Tuple typeinfo Span (Expr identifier typeannotation typeinfo binaryopsallowed) (Expr identifier typeannotation typeinfo binaryopsallowed)
+    | Expr'Tuple type_info Span (Expr identifier type_expr type_info binary_ops_allowed) (Expr identifier type_expr type_info binary_ops_allowed)
 
-    | Expr'Lambda typeinfo Span (Pattern identifier typeinfo) (Expr identifier typeannotation typeinfo binaryopsallowed)
+    | Expr'Lambda type_info Span (Pattern identifier type_info) (Expr identifier type_expr type_info binary_ops_allowed)
 
-    | Expr'Let typeinfo Span [Binding identifier typeannotation typeinfo binaryopsallowed] (Expr identifier typeannotation typeinfo binaryopsallowed)
+    | Expr'Let type_info Span [Binding identifier type_expr type_info binary_ops_allowed] (Expr identifier type_expr type_info binary_ops_allowed)
 
-    | Expr'BinaryOps binaryopsallowed typeinfo Span (Expr identifier typeannotation typeinfo binaryopsallowed) [(identifier, Expr identifier typeannotation typeinfo binaryopsallowed)]
+    | Expr'BinaryOps binary_ops_allowed type_info Span (Expr identifier type_expr type_info binary_ops_allowed) [(identifier, Expr identifier type_expr type_info binary_ops_allowed)]
 
-    | Expr'Call typeinfo Span (Expr identifier typeannotation typeinfo binaryopsallowed) (Expr identifier typeannotation typeinfo binaryopsallowed)
+    | Expr'Call type_info Span (Expr identifier type_expr type_info binary_ops_allowed) (Expr identifier type_expr type_info binary_ops_allowed)
 
-    | Expr'If typeinfo Span Span (Expr identifier typeannotation typeinfo binaryopsallowed) (Expr identifier typeannotation typeinfo binaryopsallowed) (Expr identifier typeannotation typeinfo binaryopsallowed)
-    | Expr'Case typeinfo Span Span (Expr identifier typeannotation typeinfo binaryopsallowed) [(Pattern identifier typeinfo, Expr identifier typeannotation typeinfo binaryopsallowed)]
+    | Expr'If type_info Span Span (Expr identifier type_expr type_info binary_ops_allowed) (Expr identifier type_expr type_info binary_ops_allowed) (Expr identifier type_expr type_info binary_ops_allowed)
+    | Expr'Case type_info Span Span (Expr identifier type_expr type_info binary_ops_allowed) [(Pattern identifier type_info, Expr identifier type_expr type_info binary_ops_allowed)]
 
-    | Expr'TypeAnnotation typeinfo Span typeannotation (Expr identifier typeannotation typeinfo binaryopsallowed)
+    | Expr'TypeAnnotation type_info Span type_expr (Expr identifier type_expr type_info binary_ops_allowed)
 
-    | Expr'Poison typeinfo Span
+    | Expr'Poison type_info Span
     deriving Show
 
-data Pattern identifier typeinfo
-    = Pattern'Identifier typeinfo Span BoundValueKey
-    | Pattern'Wildcard typeinfo Span
-    | Pattern'Tuple typeinfo Span (Pattern identifier typeinfo) (Pattern identifier typeinfo)
-    | Pattern'Named typeinfo Span Span (Located BoundValueKey) (Pattern identifier typeinfo)
+data Pattern identifier type_info
+    = Pattern'Identifier type_info Span BoundValueKey
+    | Pattern'Wildcard type_info Span
+    | Pattern'Tuple type_info Span (Pattern identifier type_info) (Pattern identifier type_info)
+    | Pattern'Named type_info Span Span (Located BoundValueKey) (Pattern identifier type_info)
 
-    | Pattern'Poison typeinfo Span
+    | Pattern'Poison type_info Span
     deriving Show
 
-expr_type :: Expr identifier typeannotation typeinfo binaryopsallowed -> typeinfo
-expr_type (Expr'Identifier typeinfo _ _) = typeinfo
-expr_type (Expr'Char typeinfo _ _) = typeinfo
-expr_type (Expr'String typeinfo _ _) = typeinfo
-expr_type (Expr'Int typeinfo _ _) = typeinfo
-expr_type (Expr'Float typeinfo _ _) = typeinfo
-expr_type (Expr'Bool typeinfo _ _) = typeinfo
-expr_type (Expr'Tuple typeinfo _ _ _) = typeinfo
-expr_type (Expr'Lambda typeinfo _ _ _) = typeinfo
-expr_type (Expr'Let typeinfo _ _ _) = typeinfo
-expr_type (Expr'BinaryOps _ typeinfo _ _ _) = typeinfo
-expr_type (Expr'Call typeinfo _ _ _) = typeinfo
-expr_type (Expr'If typeinfo _ _ _ _ _) = typeinfo
-expr_type (Expr'Case typeinfo _ _ _ _) = typeinfo
-expr_type (Expr'Poison typeinfo _) = typeinfo
-expr_type (Expr'TypeAnnotation typeinfo _ _ _) = typeinfo
+expr_type :: Expr identifier type_expr type_info binary_ops_allowed -> type_info
+expr_type (Expr'Identifier type_info _ _) = type_info
+expr_type (Expr'Char type_info _ _) = type_info
+expr_type (Expr'String type_info _ _) = type_info
+expr_type (Expr'Int type_info _ _) = type_info
+expr_type (Expr'Float type_info _ _) = type_info
+expr_type (Expr'Bool type_info _ _) = type_info
+expr_type (Expr'Tuple type_info _ _ _) = type_info
+expr_type (Expr'Lambda type_info _ _ _) = type_info
+expr_type (Expr'Let type_info _ _ _) = type_info
+expr_type (Expr'BinaryOps _ type_info _ _ _) = type_info
+expr_type (Expr'Call type_info _ _ _) = type_info
+expr_type (Expr'If type_info _ _ _ _ _) = type_info
+expr_type (Expr'Case type_info _ _ _ _) = type_info
+expr_type (Expr'Poison type_info _) = type_info
+expr_type (Expr'TypeAnnotation type_info _ _ _) = type_info
 
-expr_span :: Expr identifier typeannotation typeinfo binaryopsallowed -> Span
+expr_span :: Expr identifier type_expr type_info binary_ops_allowed -> Span
 expr_span (Expr'Identifier _ sp _) = sp
 expr_span (Expr'Char _ sp _) = sp
 expr_span (Expr'String _ sp _) = sp
@@ -120,14 +120,14 @@ expr_span (Expr'Case _ sp _ _ _) = sp
 expr_span (Expr'Poison _ sp) = sp
 expr_span (Expr'TypeAnnotation _ sp _ _) = sp
 
-pattern_type :: Pattern typeannotation typeinfo -> typeinfo
-pattern_type (Pattern'Identifier typeinfo _ _) = typeinfo
-pattern_type (Pattern'Wildcard typeinfo _) = typeinfo
-pattern_type (Pattern'Tuple typeinfo _ _ _) = typeinfo
-pattern_type (Pattern'Named typeinfo _ _ _ _) = typeinfo
-pattern_type (Pattern'Poison typeinfo _) = typeinfo
+pattern_type :: Pattern type_expr type_info -> type_info
+pattern_type (Pattern'Identifier type_info _ _) = type_info
+pattern_type (Pattern'Wildcard type_info _) = type_info
+pattern_type (Pattern'Tuple type_info _ _ _) = type_info
+pattern_type (Pattern'Named type_info _ _ _ _) = type_info
+pattern_type (Pattern'Poison type_info _) = type_info
 
-pattern_span :: Pattern typeannotation typeinfo -> Span
+pattern_span :: Pattern type_expr type_info -> Span
 pattern_span (Pattern'Identifier _ sp _) = sp
 pattern_span (Pattern'Wildcard _ sp) = sp
 pattern_span (Pattern'Tuple _ sp _ _) = sp
---
+++ src/UHF/Phases/Middle/Type/SolveConstraints.hs
@@ -0,0 +1,151 @@
+module UHF.Phases.Middle.Type.SolveConstraints (solve) where
+
+import UHF.Util.Prelude
+
+import qualified Arena
+import qualified UHF.Data.IR.Type as Type
+
+import qualified UHF.Compiler as Compiler
+
+import UHF.IO.Located (Located (..))
+
+import UHF.Phases.Middle.Type.Var
+import UHF.Phases.Middle.Type.Aliases
+import UHF.Phases.Middle.Type.Error
+import UHF.Phases.Middle.Type.Constraint
+import UHF.Phases.Middle.Type.StateWithVars
+
+type TypeContextReader = ReaderT (TypedWithVarsADTArena, TypedWithVarsTypeSynonymArena)
+
+read_adts :: Applicative m => TypeContextReader m TypedWithVarsADTArena
+read_adts = ReaderT (\ (adts, _) -> pure adts)
+read_synonyms :: Applicative m => TypeContextReader m TypedWithVarsTypeSynonymArena
+read_synonyms = ReaderT (\ (_, synonyms) -> pure synonyms)
+
+solve :: TypedWithVarsADTArena -> TypedWithVarsTypeSynonymArena -> [Constraint] -> StateWithVars ()
+solve adts type_synonyms constraints = runReaderT (mapM_ solve' constraints) (adts, type_synonyms)
+
+-- TODO: figure out how to gracefully handle errors because the type variables become ambiguous if they cant be unified
+solve' :: Constraint -> TypeContextReader StateWithVars ()
+solve' (Eq in_what sp a b) =
+    runExceptT (unify (unlocate a) (unlocate b)) >>= \case
+        Right () -> pure ()
+
+        Left (Left (a_part, b_part)) -> -- mismatch error
+            lift get >>= \ vars ->
+            read_adts >>= \ adts ->
+            read_synonyms >>= \ type_synonyms ->
+            lift (lift $ Compiler.tell_error $ EqError { eq_error_adts = adts, eq_error_type_synonyms = type_synonyms, eq_error_vars = vars, eq_error_in_what = in_what, eq_error_span = sp, eq_error_a_whole = a, eq_error_b_whole = b, eq_error_a_part = a_part, eq_error_b_part = b_part }) >>
+            pure ()
+
+        Left (Right (var, ty)) -> -- occurs check failure
+            lift get >>= \ vars ->
+            read_adts >>= \ adts ->
+            read_synonyms >>= \ type_synonyms ->
+            lift (lift $ Compiler.tell_error $ OccursCheckError adts type_synonyms vars sp var ty) >>
+            pure ()
+
+solve' (Expect in_what got expect) =
+    runExceptT (unify (unlocate got) expect) >>= \case
+        Right () -> pure ()
+
+        Left (Left (got_part, expect_part)) -> -- mismatch error
+            lift get >>= \ vars ->
+            read_adts >>= \ adts ->
+            read_synonyms >>= \ type_synonyms ->
+            lift (lift $ Compiler.tell_error $ ExpectError { expect_error_adts = adts, expect_error_type_synonyms = type_synonyms, expect_error_vars = vars, expect_error_in_what = in_what, expect_error_got_whole = got, expect_error_expect_whole = expect, expect_error_got_part = got_part, expect_error_expect_part = expect_part }) >>
+            pure ()
+
+        Left (Right (var, ty)) -> -- occurs check failure
+            lift get >>= \ vars ->
+            read_adts >>= \ adts ->
+            read_synonyms >>= \ type_synonyms ->
+            lift (lift $ Compiler.tell_error $ OccursCheckError adts type_synonyms vars (just_span got) var ty) >>
+            pure ()
+
+unify :: TypeWithVars -> TypeWithVars -> ExceptT (Either (TypeWithVars, TypeWithVars) (TypeVarKey, TypeWithVars)) (TypeContextReader StateWithVars) ()
+unify a@(Type.Type'ADT a_adt_key) b@(Type.Type'ADT b_adt_key)
+    | a_adt_key == b_adt_key = pure ()
+    | otherwise = ExceptT (pure $ Left $ Left (a, b))
+
+unify (Type.Type'Synonym a_syn_key) b =
+    lift read_synonyms >>= \ type_synonyms ->
+    case Arena.get type_synonyms a_syn_key of
+        Type.TypeSynonym _ a_expansion -> unify a_expansion b
+
+unify a (Type.Type'Synonym b_syn_key) =
+    lift read_synonyms >>= \ type_synonyms ->
+    case Arena.get type_synonyms b_syn_key of
+        Type.TypeSynonym _ b_expansion -> unify a b_expansion
+
+unify (Type.Type'Variable a) b = unify_var a b False
+unify a (Type.Type'Variable b) = unify_var b a True
+unify Type.Type'Int Type.Type'Int = pure ()
+unify Type.Type'Float Type.Type'Float = pure ()
+unify Type.Type'Char Type.Type'Char = pure ()
+unify Type.Type'String Type.Type'String = pure ()
+unify Type.Type'Bool Type.Type'Bool = pure ()
+unify (Type.Type'Function a1 r1) (Type.Type'Function a2 r2) = unify a1 a2 >> unify r1 r2
+unify (Type.Type'Tuple a1 b1) (Type.Type'Tuple a2 b2) = unify a1 a2 >> unify b1 b2
+unify a b = ExceptT (pure $ Left $ Left (a, b))
+
+unify_var :: TypeVarKey -> TypeWithVars -> Bool -> ExceptT (Either (TypeWithVars, TypeWithVars) (TypeVarKey, TypeWithVars)) (TypeContextReader StateWithVars) ()
+unify_var var other var_on_right = Arena.get <$> lift (lift get) <*> pure var >>= \ case
+    -- if this variable can be expanded, unify its expansion
+    TypeVar _ (Substituted var_sub) ->
+        if var_on_right
+            then unify other var_sub
+            else unify var_sub other
+
+    -- if this variable has no substitution, what happens depends on the other type
+    TypeVar _ Fresh ->
+        case other of
+            Type.Type'Variable other_var ->
+                Arena.get <$> lift (lift get) <*> pure other_var >>= \case
+                    -- if the other type is a substituted type variable, unify this variable with the other's expansion
+                    TypeVar _ (Substituted other_var_sub) -> unify_var var other_var_sub var_on_right
+
+                    -- if the other type is a fresh type variable, both of them are fresh type variables and the only thing that can be done is to unify them
+                    TypeVar _ Fresh ->
+                        if var /= other_var
+                            then (lift (set_type_var_state var (Substituted other)))
+                            else pure ()
+
+            -- if the other type is a type and not a variable
+            _ -> lift (occurs_check var other) >>= \case
+                True -> ExceptT (pure $ Left $ Right (var, other))
+                False -> lift (set_type_var_state var (Substituted other))
+
+set_type_var_state :: TypeVarKey -> TypeVarState -> TypeContextReader StateWithVars ()
+set_type_var_state var new_state = lift $ modify $ \ ty_arena -> Arena.modify ty_arena var (\ (TypeVar for _) -> TypeVar for new_state)
+
+occurs_check :: TypeVarKey -> TypeWithVars -> TypeContextReader StateWithVars Bool
+-- does the variable v occur anywhere in the type ty?
+occurs_check v ty =
+    case ty of
+        Type.Type'Variable other_v ->
+            if v == other_v
+                then pure True
+                else
+                    Arena.get <$> lift get <*> pure other_v >>= \case
+                        TypeVar _ (Substituted other_sub) -> occurs_check v other_sub
+                        TypeVar _ Fresh -> pure False
+
+        Type.Type'ADT adt_key ->
+            read_adts >>= \ adts ->
+            case Arena.get adts adt_key of
+                Type.ADT _ _ -> pure False -- TODO: check type arguemnts when those are added
+
+        Type.Type'Synonym syn_key ->
+            read_synonyms >>= \ type_synonyms ->
+            case Arena.get type_synonyms syn_key of
+                Type.TypeSynonym _ other_expansion -> occurs_check v other_expansion
+
+        Type.Type'Int -> pure False
+        Type.Type'Float -> pure False
+        Type.Type'Char -> pure False
+        Type.Type'String -> pure False
+        Type.Type'Bool -> pure False
+        Type.Type'Function a r -> (||) <$> occurs_check v a <*> occurs_check v r
+        Type.Type'Tuple a b -> (||) <$> occurs_check v a <*> occurs_check v b
+

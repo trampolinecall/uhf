050d03f023c990d975f6b049f9dfb13ec9d266bd
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-03-14 19:42:47 -0400

    implement mangling

--- src/UHF/Data/IR/ID.hs
+++ src/UHF/Data/IR/ID.hs
@@ -24,6 +24,8 @@ module UHF.Data.IR.ID
 import UHF.Util.Prelude
 
 import qualified Data.Text as Text
+import qualified Data.Char as Char
+import qualified Numeric
 
 data ModuleID = ModuleID [Text] deriving Show
 
@@ -49,7 +51,6 @@ data ExprIDSegment
     | CaseArm Int
     | TypeAnnotationExpr
     | ExprTupleRight
-    | LambdaLet
     | LambdaReferParam
     deriving Show
 
@@ -135,62 +136,95 @@ stringify = stringify' . to_general_id
         stringify_expr_segment (CaseArm i) = "arm" <> show i
         stringify_expr_segment (TypeAnnotationExpr) = "expr"
         stringify_expr_segment (ExprTupleRight) = "right"
-        stringify_expr_segment (LambdaLet) = "let"
         stringify_expr_segment (LambdaReferParam) = "param"
 
         stringify_pat_segment (PatTupleItem i) = "item" <> show i
         stringify_pat_segment (PatTupleRight) = "right"
         stringify_pat_segment (NamedPatOther) = "other"
 
+class Mangle m where
+    mangle' :: m -> Text
+
+instance Mangle GeneralID where
+    mangle' (GM m) = "m" <> mangle' m
+    mangle' (GD d) = "d" <> mangle' d
+    mangle' (GE e) = "e" <> mangle' e
+    mangle' (GP p) = "p" <> mangle' p
+    mangle' (GBV bv) = "bv" <> mangle' bv
+
+instance Mangle ModuleID where
+    mangle' (ModuleID path) = mangle' path
+
+instance Mangle DeclID where
+    mangle' (DeclID parent name) = mangle' parent <> mangle' name
+instance Mangle DeclParent where
+    mangle' (DeclParent'Module m) = "m" <> mangle' m
+    mangle' (DeclParent'Expr e) = "e" <> mangle' e
+
+instance Mangle ExprID where
+    mangle' (ExprID parent pieces) = mangle' parent <> mangle' pieces
+instance Mangle ExprParent where
+    mangle' (ExprParent'Binding db ind) = "b" <> mangle' db <> mangle' ind
+    mangle' (ExprParent'CaseArm e ind) = "c" <> mangle' e <> mangle' ind
+    mangle' (ExprParent'TupleDestructureL p) = "l" <> mangle' p
+    mangle' (ExprParent'TupleDestructureR p) = "r" <> mangle' p
+    mangle' (ExprParent'NamedRefer p) = "n" <> mangle' p
+instance Mangle ExprIDSegment where
+    mangle' (ExprTupleItem i) = "t" <> mangle' i
+    mangle' (LambdaParam) = "p"
+    mangle' (LambdaBody) = "b"
+    mangle' (LetResult) = "r"
+    mangle' (BinaryOpsArg i) = "o" <> mangle' i
+    mangle' (CallCallee) = "c"
+    mangle' (CallArg i) = "a" <> mangle' i
+    mangle' (IfCond) = "d" -- d is the letter after c
+    mangle' (IfTrue) = "u" -- u is the letter after t
+    mangle' (IfFalse) = "f"
+    mangle' (CaseTest) = "v" -- v is the second letter after t
+    mangle' (CaseArm i) = "e" <> mangle' i -- e is the fourth letter after a
+    mangle' (TypeAnnotationExpr) = "w" -- w is the third letter after t
+    mangle' (ExprTupleRight) = "s" -- s is the letter after r
+    mangle' (LambdaReferParam) = "q" -- q is the letter after p
+
+instance Mangle PatID where
+    mangle' (PatID parent pieces) = mangle' parent <> mangle' pieces
+instance Mangle PatParent where
+    mangle' (PatParent'Binding db ind) = "b" <> mangle' db <> mangle' ind
+    mangle' (PatParent'CaseArm e ind) = "c" <> mangle' e <> mangle' ind
+    mangle' (PatParent'LambdaParam e ind) = "l" <> mangle' e <> mangle' ind
+instance Mangle PatIDSegment where
+    mangle' (PatTupleItem i) = "t" <> mangle' i
+    mangle' (PatTupleRight) = "r"
+    mangle' (NamedPatOther) = "o"
+
+instance Mangle BoundValueID where
+    mangle' (BoundValueID parent pieces) = "b" <> mangle' parent <> mangle' pieces
+    mangle' (BoundValueID'MadeUpPat p) = "p" <> mangle' p
+    mangle' (BoundValueID'MadeUpTupleLeft t) = "l" <> mangle' t
+    mangle' (BoundValueID'MadeUpTupleRight t) = "r" <> mangle' t
+    mangle' (BoundValueID'MadeUpLambdaParam e) = "m" <> mangle' e -- m is the letter after l
+
+instance Mangle BoundValueParent where
+    mangle' (BVParent'Module mod) = "m" <> mangle' mod
+    mangle' (BVParent'LambdaParam lam) = "l" <> mangle' lam
+    mangle' (BVParent'Let e) = "m" <> mangle' e -- m is the letter after l
+    mangle' (BVParent'CaseArm e ind) = "c" <> mangle' e <> mangle' ind
+
+instance Mangle a => Mangle [a] where
+    mangle' things = show (length things) <> "_" <> Text.concat (map mangle' things)
+
+instance Mangle Text where
+    mangle' t = show (Text.length t) <> "_" <> Text.concatMap encode_char t
+        where
+            encode_char '_' = "__"
+            encode_char c
+                | Char.isAscii c = Text.singleton c
+                | otherwise =
+                    let code = Text.pack $ Numeric.showHex (Char.ord c) ""
+                    in "_" <> Text.replicate (4 - Text.length code) "0" <> code
+
+instance Mangle Int where
+    mangle' i = mangle' (show i :: Text)
+
 mangle :: ID i => i -> Text
 mangle = mangle' . to_general_id
-    where
-        mangle' (GM (ModuleID [])) = "root"
-        mangle' (GM (ModuleID segments)) = Text.intercalate "::" segments
-        mangle' (GD (DeclID parent name)) = mangle_decl_parent parent <> "::" <> name
-        mangle' (GE (ExprID parent segments)) = mangle_expr_parent parent <> Text.concat (map (("::"<>) . mangle_expr_segment) segments)
-        mangle' (GP (PatID parent segments)) = mangle_pat_parent parent <> Text.concat (map (("::"<>) . mangle_pat_segment) segments)
-        mangle' (GBV (BoundValueID bv_parent t)) = mangle_bv_parent bv_parent <> "::" <> t
-        mangle' (GBV (BoundValueID'MadeUpPat pat)) = mangle' (GP pat)
-        mangle' (GBV (BoundValueID'MadeUpTupleLeft pat)) = mangle' (GP pat) <> "_left"
-        mangle' (GBV (BoundValueID'MadeUpTupleRight pat)) = mangle' (GP pat) <> "_right"
-        mangle' (GBV (BoundValueID'MadeUpLambdaParam ex)) = mangle' (GE ex) <> "_param"
-
-        mangle_bv_parent (BVParent'Module mod) = mangle' (GM mod)
-        mangle_bv_parent (BVParent'Let e) = mangle' (GE e)
-        mangle_bv_parent (BVParent'LambdaParam e) = mangle' (GE e)
-        mangle_bv_parent (BVParent'CaseArm e i) = mangle' (GE e) <> "::arm" <> show i
-
-        mangle_decl_parent (DeclParent'Module mod) = mangle' (GM mod)
-        mangle_decl_parent (DeclParent'Expr e) = mangle' (GE e)
-
-        mangle_expr_parent (ExprParent'Binding decl_parent ind) = mangle_decl_parent decl_parent <> "::binding" <> show ind
-        mangle_expr_parent (ExprParent'CaseArm expr ind) = mangle' (GE expr) <> "::arm" <> show ind
-        mangle_expr_parent (ExprParent'TupleDestructureL pat) = mangle' (GP pat) <> "::destructure_l"
-        mangle_expr_parent (ExprParent'TupleDestructureR pat) = mangle' (GP pat) <> "::destructure_r"
-        mangle_expr_parent (ExprParent'NamedRefer pat) = mangle' (GP pat)
-
-        mangle_pat_parent (PatParent'Binding decl_parent ind) = mangle_decl_parent decl_parent <> "::binding" <> show ind
-        mangle_pat_parent (PatParent'CaseArm expr ind) = mangle' (GE expr) <> "::arm" <> show ind
-        mangle_pat_parent (PatParent'LambdaParam expr ind) = mangle' (GE expr) <> "::param" <> show ind
-
-        mangle_expr_segment (ExprTupleItem i) = "tuple_item" <> show i
-        mangle_expr_segment (LambdaParam) = "param"
-        mangle_expr_segment (LambdaBody) = "body"
-        mangle_expr_segment (LetResult) = "result"
-        mangle_expr_segment (BinaryOpsArg i) = "arg" <> show i
-        mangle_expr_segment (CallCallee) = "callee"
-        mangle_expr_segment (CallArg i) = "arg" <> show i
-        mangle_expr_segment (IfCond) = "cond"
-        mangle_expr_segment (IfTrue) = "true_branch"
-        mangle_expr_segment (IfFalse) = "false_branch"
-        mangle_expr_segment (CaseTest) = "test"
-        mangle_expr_segment (CaseArm i) = "arm" <> show i
-        mangle_expr_segment (TypeAnnotationExpr) = "expr"
-        mangle_expr_segment (ExprTupleRight) = "right"
-        mangle_expr_segment (LambdaLet) = "let"
-        mangle_expr_segment (LambdaReferParam) = "param"
-
-        mangle_pat_segment (PatTupleItem i) = "item" <> show i
-        mangle_pat_segment (PatTupleRight) = "right"
-        mangle_pat_segment (NamedPatOther) = "other"

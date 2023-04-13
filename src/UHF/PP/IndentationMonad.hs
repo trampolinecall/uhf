a4fcfd9a65e6e065a5342330966d4ef655a14d5a
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-04-13 11:21:06 -0400

    somewhat optimize indentation monad

--- src/UHF/PP/IndentationMonad.hs
+++ src/UHF/PP/IndentationMonad.hs
@@ -2,22 +2,23 @@
 
 module UHF.PP.IndentationMonad
     ( PP
-    , run_pp
     , exec_pp
 
     , first_on_line
     , is_multiline
     , write
-    , write_ch
     , indent
     , dedent
     ) where
 
 -- TODO: write tests
 
-import UHF.Util.Prelude
+import UHF.Util.Prelude hiding (StateT, runStateT, evalStateT, get, modify, Writer, runWriter, tell, execWriter)
 
 import qualified Data.Text as Text
+
+import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Writer.CPS
 
 data IndentState = IndentState Int Bool
 newtype PP a = PP (StateT IndentState (Writer Text) a) deriving (Functor, Applicative, Monad)
@@ -28,28 +29,28 @@ is_multiline = Text.any (=='\n') . exec_pp
 first_on_line :: PP Bool
 first_on_line = PP $ get >>= \ (IndentState _ is_first) -> pure is_first
 
-run_pp :: PP a -> (a, Text)
-run_pp (PP d) = runWriter $ evalStateT d (IndentState 0 True)
-
 exec_pp :: PP a -> Text
-exec_pp = snd . run_pp
+exec_pp (PP d) = execWriter $ evalStateT d (IndentState 0 True)
 
 write :: Text -> PP ()
-write t = mapM_ write_ch $ Text.unpack t
-
-output :: Char -> PP ()
-output = PP . lift . tell . Text.singleton
-
-write_ch :: Char -> PP ()
-write_ch '\n' = output '\n' >> PP (modify (\ (IndentState cur_indent _) -> IndentState cur_indent True))
-write_ch c =
-    PP get >>= (\case
-        IndentState _ True -> put_indent
-        _ -> pure ()) >>
-    PP (modify $ \ (IndentState cur_indent _) -> IndentState cur_indent False) >>
-    output c
+write t =
+    let chunks = Text.split (=='\n') t
+    in mapM_ put_chunk $ intersperse Nothing (map Just chunks)
     where
-        put_indent = PP $ get >>= \ (IndentState indent _) -> lift (tell $ Text.replicate indent " ")
+        put_chunk Nothing =
+            PP (lift $ tell "\n") >>
+            PP (modify (\ (IndentState cur_indent _) -> IndentState cur_indent True))
+
+        put_chunk (Just c)
+            | Text.null c = pure ()
+            | otherwise =
+                PP get >>= (\case
+                    IndentState _ True -> put_indent
+                    _ -> pure ()) >>
+                PP (modify $ \ (IndentState cur_indent _) -> IndentState cur_indent False) >>
+                PP (lift $ tell c)
+
+        put_indent = PP $ get >>= \ (IndentState indent _) -> lift (tell $ Text.replicate indent (Text.singleton ' '))
 
 indent :: PP ()
 indent = PP $ modify $ \ (IndentState indent at_start) -> IndentState (indent + 4) at_start

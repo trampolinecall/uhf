b0c8d3b66c8aa84f4c50a1cfefa0699987e6c118
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-05-28 12:26:28 -0400

    add modules for typescript syntax

---
+++ src/UHF/Phases/Back/TSBackend/TS.hs
@@ -0,0 +1,9 @@
+module UHF.Phases.Back.TSBackend.TS (Stmt (..), Expr (..), Type (..)) where
+
+import UHF.Util.Prelude
+
+-- a subset of the typescript ast that is actually used by the backend
+
+data Stmt
+data Expr
+data Type

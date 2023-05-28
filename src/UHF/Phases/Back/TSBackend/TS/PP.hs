1863fb0da6357c356b0fe9875074ebb37c9528f7
trampolinecall <>
2023-05-28 12:26:28 -0400

    add modules for typescript syntax

---
+++ src/UHF/Phases/Back/TSBackend/TS/PP.hs
@@ -0,0 +1,10 @@
+module UHF.Phases.Back.TSBackend.TS.PP (stmts) where
+
+import UHF.Util.Prelude
+
+import qualified UHF.PP as PP
+
+import qualified UHF.Phases.Back.TSBackend.TS as TS
+
+stmts :: [TS.Stmt] -> Text
+stmts = todo

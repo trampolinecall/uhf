9b8a41e685d6f267201b7376172e7ca618031f3c
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-05-30 19:13:55 -0400

    fix precedence for expressions

--- src/UHF/Phases/Back/TSBackend/TS/PP.hs
+++ src/UHF/Phases/Back/TSBackend/TS/PP.hs
@@ -36,17 +36,83 @@ stmt (TS.Stmt'Return e) = PP.List ["return ", expr e, ";"]
 stmt (TS.Stmt'Expr e) = PP.List [expr e, ";"]
 
 expr :: TS.Expr -> PP.Token
-expr e = PP.List ["(", expr' e, ")"] -- TODO: also do precedence correctly
+expr = level1
     where
-        expr' (TS.Expr'Identifier n) = PP.String n
-        expr' (TS.Expr'Int i) = PP.String $ show i
-        expr' (TS.Expr'Bool b) = if b then "true" else "false"
-        expr' (TS.Expr'Char c) = PP.String $ show c
-        expr' (TS.Expr'String s) = PP.String $ show s
-        expr' (TS.Expr'Undefined) = "undefined"
-        expr' (TS.Expr'StrLit s) = PP.List ["\"", PP.String s, "\""]
-        expr' (TS.Expr'List es) = PP.bracketed_comma_list PP.Inconsistent (map expr es)
-        expr' (TS.Expr'Object items) =
+        -- "one simple trick" for printing precedence taken from https://www.haskellforall.com/2020/11/pretty-print-syntax-trees-with-this-one.html
+        -- precedence info is taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table, also is not complete
+
+        -- level 1: comma / sequence operator (not in this ast)
+        level1 = level2
+
+        -- level 2: '=>' rtl, '?:' rtl, assignment rtl
+        level2 (TS.Expr'ArrowFunction p ret body) = PP.List [params p, type_annotation ret, " => ", body']
+            where
+                body' = case body of
+                    Left e -> level2 e
+                    Right stmts -> PP.braced_block $ map stmt stmts
+        level2 (TS.Expr'Assign lhs rhs) = PP.List [level3 lhs, " = ", level2 rhs]
+        level2 e = level3 e
+
+        -- level 3: '||' ltr, '??' ltr
+        level3 = level4
+
+        -- level 4: '&&' ltr
+        level4 = level5
+
+        -- level 5: '|' ltr
+        level5 = level6
+
+        -- level 6: '^' ltr
+        level6 = level7
+
+        -- level 7: '&' ltr
+        level7 = level8
+
+        -- level 8: equality ltr
+        level8 = level9
+
+        -- level 9: comparison ltr, 'in' ltr, 'instanceof' ltr
+        level9 = level10
+
+        -- level 10: bitwise shift ltr
+        level10 = level11
+
+        -- level 11: addition and subtraction ltr
+        level11 = level12
+
+        -- level 12: multiplication, division, modulo ltr
+        level12 (TS.Expr'Div a b) = PP.List [level12 a, " / ", level13 b]
+        level12 e = level13 e
+
+        -- level 13: exponentiation ('**') rtl
+        level13 = level14
+
+        -- level 14: '!x', '~x', '+x', '-x', '++x', '--x', 'typeof x', 'void x', 'delete x', 'await x'
+        level14 = level15
+
+        -- level 15: 'x++', 'y++'
+        level15 = level16
+
+        -- level 16: 'new x'
+        level16 (TS.Expr'New constructor []) = PP.List ["new ", level17 constructor] -- not sure what this is supposed to be
+        level16 e = level17 e
+
+        -- level 17: 'x[y]', 'new x()', function call, also 'x.y' and 'x?.y' ltr
+        level17 (TS.Expr'Get e field) = PP.List [level17 e, ".", PP.String field]
+        level17 (TS.Expr'Call e args) = PP.List [level17 e, PP.parenthesized_comma_list PP.Inconsistent (map expr args)]
+        level17 (TS.Expr'New constructor args) = PP.List ["new ", level18 constructor, PP.parenthesized_comma_list PP.Inconsistent (map expr args)]
+        level17 e = level18 e
+
+        -- level 18: parentheses
+        level18 (TS.Expr'Identifier n) = PP.String n
+        level18 (TS.Expr'Int i) = PP.String $ show i
+        level18 (TS.Expr'Bool b) = if b then "true" else "false"
+        level18 (TS.Expr'Char c) = PP.String $ show c
+        level18 (TS.Expr'String s) = PP.String $ show s
+        level18 (TS.Expr'Undefined) = "undefined"
+        level18 (TS.Expr'StrLit s) = PP.List ["\"", PP.String s, "\""]
+        level18 (TS.Expr'List es) = PP.bracketed_comma_list PP.Inconsistent (map expr es)
+        level18 (TS.Expr'Object items) =
             PP.braced_comma_list
                 PP.Inconsistent
                 (map
@@ -56,22 +122,13 @@ expr e = PP.List ["(", expr' e, ")"] -- TODO: also do precedence correctly
                                 Just e -> PP.List [": ", expr e]
                         in PP.List [PP.String n, init'])
                     items)
-        expr' (TS.Expr'ArrowFunction p ret body) = PP.List [params p, type_annotation ret, " => ", body']
-            where
-                body' = case body of
-                    Left e -> expr e
-                    Right stmts -> PP.braced_block $ map stmt stmts
-        expr' (TS.Expr'New constructor args) = PP.List ["new ", expr constructor, PP.parenthesized_comma_list PP.Inconsistent (map expr args)]
-        expr' (TS.Expr'Call e args) = PP.List [expr e, PP.parenthesized_comma_list PP.Inconsistent (map expr args)]
-        expr' (TS.Expr'Get e field) = PP.List [expr e, ".", PP.String field]
-        expr' (TS.Expr'Div a b) = PP.List [expr a, " / ", expr b]
-        expr' (TS.Expr'Assign lhs rhs) = PP.List [expr lhs, " = ", expr rhs]
+        level18 e = PP.List ["(", level1 e, ")"]
 
 type_ :: TS.Type -> PP.Token
-type_ t = level1 t
+type_ = level1
     where
         -- precedence taken from https://github.com/antlr/grammars-v4/blob/master/javascript/typescript/TypeScriptParser.g4#L80
-        -- "one simple trick" for printing precedence taken from https://www.haskellforall.com/2020/11/pretty-print-syntax-trees-with-this-one.html
+        -- same "one simple trick" used
 
         level1 (TS.Type'StrLit s) = PP.List ["\"", PP.String s, "\""]
         level1 t = level2 t

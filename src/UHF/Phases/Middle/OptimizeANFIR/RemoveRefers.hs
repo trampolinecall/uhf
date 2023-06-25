8595b0cc39030e3e21908af65d5bccc9876c2d23
Jason N <fireworkinyourbathtub@gmail.com>
2023-06-25 12:17:39 -0400

    move BoundWhere to BackendIR

--- src/UHF/Phases/Middle/OptimizeANFIR/RemoveRefers.hs
+++ src/UHF/Phases/Middle/OptimizeANFIR/RemoveRefers.hs
@@ -19,7 +19,7 @@ trace_passthrough bk = trace [] bk
             | otherwise =
                 get_binding bk >>= \case
                     -- if this expression is referring to a refer node, recursively trace through the refer node's referents and make the expression refer to that
-                    ANFIR.Binding _ (ANFIR.Expr'Refer _ _ referent) -> trace (bk:already_visited) referent
+                    ANFIR.Binding (ANFIR.Expr'Refer _ _ referent) -> trace (bk:already_visited) referent
                     _ -> pure bk
 
         get_binding bk = ask >>= \ (ANFIR.ANFIR _ _ _ _ bindings _ _) -> pure (Arena.get bindings bk)

5883a79ec813c534a493befa5b2a706307383301
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-11-22 20:20:00 -0500

    remove Showable from typescript

--- data/ts_runtime.ts
+++ data/ts_runtime.ts
@@ -2,52 +2,22 @@ interface Lambda<A, R> {
     call(arg: A): R;
 }
 
-// TODO: make this a typeclass in uhf
-interface Showable {
-    show(): string;
-}
-
 class Int {
     constructor(public value: number) {}
-
-    show(): string {
-        return this.value.toString();
-    }
 }
 class Float {
     constructor(public value: number) {}
-
-    show(): string {
-        return this.value.toString();
-    }
 }
 class Char {
     constructor(public value: string) {}
-
-    show(): string {
-        return `'${this.value}'`;
-    }
 }
 class UHFString {
     constructor(public value: string) {}
-
-    show(): string {
-        return `"${this.value.toString()}"`;
-    }
 }
 class Bool {
     constructor(public value: boolean) {}
-
-    show(): string {
-        return this.value.toString();
-    }
 }
-// TODO: this should not have these bounds on the parameters
-class Tuple<A extends Showable, B extends Showable> {
+class Tuple<A, B> {
     constructor(public first: A, public second: B) {}
-
-    show(): string {
-        return `(${this.first.show()}, ${this.second.show()})`;
-    }
 }
 

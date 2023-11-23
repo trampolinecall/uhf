76e9f81b08d90a895a6d9a18bc31945422416c8d
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-11-22 20:05:41 -0500

    fix precedences in all pretty printers

--- src/UHF/Data/AST/PP.hs
+++ src/UHF/Data/AST/PP.hs
@@ -3,6 +3,7 @@ module UHF.Data.AST.PP (pp_decls) where
 import UHF.Util.Prelude
 
 import qualified UHF.PP as PP
+import qualified UHF.PP.Precedence as PP.Precedence
 import qualified UHF.Data.AST as AST
 
 import UHF.IO.Located (Located (..))
@@ -30,44 +31,44 @@ pp_data_variant (AST.DataVariant'Anon name fields) = PP.List [pp_iden name, PP.p
 pp_data_variant (AST.DataVariant'Named name fields) = PP.List [pp_iden name, " ", PP.braced_block $ map (\ (name, ty) -> PP.List [pp_iden name, ": ", pp_type ty, ";"]) fields, ";"]
 
 pp_type :: AST.Type -> PP.Token
-pp_type = level1
+pp_type = PP.Precedence.pp_precedence levels PP.Precedence.parenthesize
     where
-        -- TODO: fix infinite recursion when adding new thing and forgetting to add to here
-        level1 (AST.Type'Forall _ names subty) = PP.List ["#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_iden names, " ", level1 subty]
-        level1 t = level2 t
+        levels (AST.Type'Forall _ names subty) = (1, \ cur _ -> PP.List ["#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_iden names, " ", cur subty])
+        levels (AST.Type'Function _ arg res) = (2, \ cur next -> PP.List [next arg, " -> ", cur res])
+        levels (AST.Type'Apply _ callee args) = (3, \ cur _ -> PP.List [cur callee, "#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_type args])
+        levels (AST.Type'Identifier iden) = (4, \ _ _ -> pp_iden iden)
+        levels (AST.Type'Tuple _ items) = (4, \ _ _ -> PP.parenthesized_comma_list PP.Inconsistent $ map pp_type items)
+        levels (AST.Type'Hole _ name) = (4, \ _ _ -> PP.List ["?", pp_iden name])
+        levels (AST.Type'Wild _) = (4, \ _ _ -> PP.List ["_"])
 
-        level2 (AST.Type'Function _ arg res) = PP.List [level3 arg, " -> ", level2 res]
-        level2 t = level3 t
-
-        level3 (AST.Type'Apply _ callee args) = PP.List [level3 callee, "#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_type args]
-        level3 t = level4 t
-
-        level4 (AST.Type'Identifier iden) = pp_iden iden
-        level4 (AST.Type'Tuple _ items) = PP.parenthesized_comma_list PP.Inconsistent $ map pp_type items
-        level4 (AST.Type'Hole _ name) = PP.List ["?", pp_iden name]
-        level4 (AST.Type'Wild _) = PP.List ["_"]
-        level4 t = PP.List ["(", pp_type t, ")"]
-
--- TODO: precedence
 pp_expr :: AST.Expr -> PP.Token
-pp_expr (AST.Expr'Identifier iden) = PP.FirstOnLineIfMultiline $ pp_iden iden
-pp_expr (AST.Expr'Char _ c) = PP.FirstOnLineIfMultiline $ PP.String $ show c
-pp_expr (AST.Expr'String _ s) = PP.FirstOnLineIfMultiline $ PP.String $ show s -- TODO: deal with escapes properly / print token?
-pp_expr (AST.Expr'Int _ i) = PP.FirstOnLineIfMultiline $ PP.String $ show i
-pp_expr (AST.Expr'Float _ (n :% d)) = PP.FirstOnLineIfMultiline $ PP.String $ "(" <> show n <> " / " <> show d <> ")"
-pp_expr (AST.Expr'Bool _ b) = PP.FirstOnLineIfMultiline $ PP.String $ if b then "true" else "false"
-pp_expr (AST.Expr'Tuple _ items) = PP.parenthesized_comma_list PP.Inconsistent $ map pp_expr items
-pp_expr (AST.Expr'Lambda _ args body) = PP.FirstOnLineIfMultiline $ PP.List ["\\ ", PP.parenthesized_comma_list PP.Inconsistent $ map pp_pattern args, " -> ", pp_expr body]
-pp_expr (AST.Expr'Let _ decls res) = pp_let "let" decls res
-pp_expr (AST.Expr'LetRec _ decls res) = pp_let "letrec" decls res
-pp_expr (AST.Expr'BinaryOps _ first ops) = PP.List [pp_expr first, PP.indented_block $ map (\ (op, rhs) -> PP.List [pp_iden op, " ", pp_expr rhs]) ops]
-pp_expr (AST.Expr'Call _ callee args) = PP.List [pp_expr callee, PP.parenthesized_comma_list PP.Inconsistent $ map pp_expr args]
-pp_expr (AST.Expr'If _ _ cond true false) = PP.FirstOnLineIfMultiline $ PP.List ["if ", pp_expr cond, " then ", pp_expr true, " else ", pp_expr false]
-pp_expr (AST.Expr'Match _ _ e arms) = PP.List ["match ", pp_expr e, " ", PP.braced_block $ map (\ (pat, expr) -> PP.List [pp_pattern pat, " -> ", pp_expr expr, ";"]) arms]
-pp_expr (AST.Expr'TypeAnnotation _ ty e) = PP.List [":", pp_type ty, ": ", pp_expr e]
-pp_expr (AST.Expr'Forall _ tys e) = PP.List ["#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_iden tys, " ", pp_expr e]
-pp_expr (AST.Expr'TypeApply _ e tys) = PP.List [pp_expr e, "#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_type tys]
-pp_expr (AST.Expr'Hole _ name) = PP.FirstOnLineIfMultiline $ PP.List ["?", pp_iden name]
+pp_expr = PP.Precedence.pp_precedence levels PP.Precedence.parenthesize
+    where
+        levels (AST.Expr'BinaryOps _ first ops) = (0, \ _ next -> PP.List [next first, PP.indented_block $ map (\ (op, rhs) -> PP.List [pp_iden op, " ", next rhs]) ops])
+
+        levels (AST.Expr'Call _ callee args) = (1, \ cur _ -> PP.List [cur callee, PP.parenthesized_comma_list PP.Inconsistent $ map pp_expr args])
+        levels (AST.Expr'TypeApply _ e tys) = (1, \ cur _ -> PP.List [cur e, "#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_type tys])
+
+        levels (AST.Expr'Identifier iden) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ pp_iden iden)
+        levels (AST.Expr'Hole _ name) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.List ["?", pp_iden name])
+        levels (AST.Expr'Char _ c) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.String $ show c)
+        levels (AST.Expr'String _ s) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.String $ show s) -- TODO: deal with escapes properly / print token?
+        levels (AST.Expr'Int _ i) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.String $ show i)
+        levels (AST.Expr'Float _ (n :% d)) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.String $ "(" <> show n <> " / " <> show d <> ")")
+        levels (AST.Expr'Bool _ b) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.String $ if b then "true" else "false")
+        levels (AST.Expr'Tuple _ items) = (2, \ _ _ -> PP.parenthesized_comma_list PP.Inconsistent $ map pp_expr items)
+
+        levels (AST.Expr'Lambda _ args body) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.List ["\\ ", PP.parenthesized_comma_list PP.Inconsistent $ map pp_pattern args, " -> ", pp_expr body])
+
+        levels (AST.Expr'Let _ decls res) = (2, \ _ _ -> pp_let "let" decls res)
+        levels (AST.Expr'LetRec _ decls res) = (2, \ _ _ -> pp_let "letrec" decls res)
+
+        levels (AST.Expr'If _ _ cond true false) = (2, \ _ _ -> PP.FirstOnLineIfMultiline $ PP.List ["if ", pp_expr cond, " then ", pp_expr true, " else ", pp_expr false])
+        levels (AST.Expr'Match _ _ e arms) = (2, \ _ _ -> PP.List ["match ", pp_expr e, " ", PP.braced_block $ map (\ (pat, expr) -> PP.List [pp_pattern pat, " -> ", pp_expr expr, ";"]) arms])
+
+        levels (AST.Expr'TypeAnnotation _ ty e) = (2, \ _ _ -> PP.List [":", pp_type ty, ": ", pp_expr e])
+
+        levels (AST.Expr'Forall _ tys e) = (2, \ _ _ -> PP.List ["#", PP.parenthesized_comma_list PP.Inconsistent $ map pp_iden tys, " ", pp_expr e])
 
 pp_let :: Text -> [AST.Decl] -> AST.Expr -> PP.Token
 pp_let let_str [decl] res = PP.FirstOnLineIfMultiline $ PP.List [PP.String let_str, " ", pp_decl decl, "\n", pp_expr res]
@@ -83,4 +84,3 @@ pp_pattern (AST.Pattern'NamedADTVariant _ variant fields) = PP.List [pp_iden var
 
 pp_iden :: Located [Located Text] -> PP.Token
 pp_iden (Located _ items) = PP.List [PP.String $ Text.intercalate "::" (map unlocate items)]
-

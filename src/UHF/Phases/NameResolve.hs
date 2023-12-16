401e53c5cf132c2267ee53bdd1d6599223986081
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-16 10:38:22 -0500

    split name resolution Utils module into multiple modules

--- src/UHF/Phases/NameResolve.hs
+++ src/UHF/Phases/NameResolve.hs
@@ -8,13 +8,13 @@ import qualified UHF.Data.SIR as SIR
 import qualified UHF.Phases.NameResolve.EvalTypeExprs as EvalTypeExprs
 import qualified UHF.Phases.NameResolve.ResolveReferStarts as ResolveReferStarts
 import qualified UHF.Phases.NameResolve.ResolveVPIdens as ResolveVPIdens
-import qualified UHF.Phases.NameResolve.Utils as Utils
+import qualified UHF.Phases.NameResolve.NameMaps as NameMaps
+import qualified UHF.Phases.NameResolve.Error as Error
 import qualified UHF.Parts.TypeSolver as TypeSolver
 
--- resolve entry point {{{1
-resolve :: SIR.SIR ResolveReferStarts.Unresolved -> Utils.WithErrors (SIR.SIR ResolveVPIdens.Resolved, TypeSolver.SolverState)
+resolve :: SIR.SIR ResolveReferStarts.Unresolved -> Error.WithErrors (SIR.SIR ResolveVPIdens.Resolved, TypeSolver.SolverState)
 resolve sir =
-    Utils.collect_child_maps sir >>= \ child_maps ->
+    NameMaps.collect_child_maps sir >>= \ child_maps ->
     ResolveReferStarts.resolve child_maps sir >>= \ sir ->
     EvalTypeExprs.eval child_maps sir >>= \ (sir, solver_state) ->
     ResolveVPIdens.resolve child_maps sir >>= \ sir ->

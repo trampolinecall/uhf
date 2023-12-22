46d07068660561df7fd267369583e4a35daf1f90
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-21 21:49:36 -0500

    add new type solver to name resolution

--- src/UHF/Phases/NameResolve.hs
+++ src/UHF/Phases/NameResolve.hs
@@ -5,17 +5,19 @@ module UHF.Phases.NameResolve
 import UHF.Prelude
 
 import qualified UHF.Data.SIR as SIR
+import qualified UHF.Parts.TypeSolver as TypeSolver
+import qualified UHF.Phases.NameResolve.AssignNameMaps as AssignNameMaps
+import qualified UHF.Phases.NameResolve.Error as Error
 import qualified UHF.Phases.NameResolve.EvalTypeExprs as EvalTypeExprs
+import qualified UHF.Phases.NameResolve.NameMaps as NameMaps
 import qualified UHF.Phases.NameResolve.ResolveReferStarts as ResolveReferStarts
 import qualified UHF.Phases.NameResolve.ResolveVPIdens as ResolveVPIdens
-import qualified UHF.Phases.NameResolve.NameMaps as NameMaps
-import qualified UHF.Phases.NameResolve.Error as Error
-import qualified UHF.Parts.TypeSolver as TypeSolver
 
-resolve :: SIR.SIR ResolveReferStarts.Unresolved -> Error.WithErrors (SIR.SIR ResolveVPIdens.Resolved, TypeSolver.SolverState)
+resolve :: SIR.SIR AssignNameMaps.Unassigned -> Error.WithErrors (SIR.SIR ResolveVPIdens.Resolved, TypeSolver.SolverState)
 resolve sir =
     NameMaps.collect_child_maps sir >>= \ child_maps ->
-    ResolveReferStarts.resolve child_maps sir >>= \ sir ->
+    AssignNameMaps.assign child_maps sir >>= \ sir ->
+    ResolveReferStarts.resolve sir >>= \ sir ->
     EvalTypeExprs.eval child_maps sir >>= \ (sir, solver_state) ->
     ResolveVPIdens.resolve child_maps sir >>= \ sir ->
     pure (sir, solver_state)

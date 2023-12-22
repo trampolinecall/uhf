7bbeafef1c2be56d4de3792d5acfa67821725ee8
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-21 22:01:47 -0500

    fix test main module

--- test/uhf.hs
+++ test/uhf.hs
@@ -16,12 +16,12 @@ main =
     defaultMain $
         testGroup
             "tests"
-            [ Arena.tests
-            , UHF.Diagnostic.Report.Messages.tests
+            [ UHF.Diagnostic.Report.Messages.tests
             , UHF.Diagnostic.Report.Utils.tests
-            , UHF.Source.Location.tests
+            , UHF.PP.Precedence.tests
             , UHF.Phases.Lexer.tests
             , UHF.Phases.Parser.PEG.tests
             , UHF.Phases.Parser.tests
-            , UHF.PP.Precedence.tests
+            , UHF.Source.Location.tests
+            , UHF.Util.Arena.tests
             ]

65550a7cf1e01dbfd2257efde45b314caee572b7
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-23 09:26:25 -0500

    add CompileTests module and first compile test

--- test/uhf.hs
+++ test/uhf.hs
@@ -2,6 +2,8 @@ module Main where
 
 import UHF.Prelude
 
+import qualified CompileTests
+
 import qualified UHF.Diagnostic.Styles.Default.RenderMessagesSection (tests)
 import qualified UHF.Diagnostic.Styles.Default.Utils (tests)
 import qualified UHF.PP.Precedence (tests)
@@ -11,15 +13,24 @@ import qualified UHF.Source.Location (tests)
 import qualified UHF.Util.Arena (tests)
 
 main :: IO ()
-main =
+main = do
+    compile_tests <- CompileTests.get_compile_tests
     defaultMain $
         testGroup
             "tests"
-            [ UHF.Diagnostic.Styles.Default.RenderMessagesSection.tests
-            , UHF.Diagnostic.Styles.Default.Utils.tests
-            , UHF.PP.Precedence.tests
-            , UHF.Parts.Lexer.tests
-            , UHF.Parts.SolveTypes.Error.InferVarNamer.tests
-            , UHF.Source.Location.tests
-            , UHF.Util.Arena.tests
+            [ unit_tests
+            , compile_tests
             ]
+
+unit_tests :: TestTree
+unit_tests =
+    testGroup
+        "tests"
+        [ UHF.Diagnostic.Styles.Default.RenderMessagesSection.tests
+        , UHF.Diagnostic.Styles.Default.Utils.tests
+        , UHF.PP.Precedence.tests
+        , UHF.Parts.Lexer.tests
+        , UHF.Parts.SolveTypes.Error.InferVarNamer.tests
+        , UHF.Source.Location.tests
+        , UHF.Util.Arena.tests
+        ]

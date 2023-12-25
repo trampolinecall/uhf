7c4aeb684d2daeabfe5b0ded6d2932dbfc2aaff0
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-25 14:24:52 -0500

    add hack to make paths work in expressions

--- src/UHF/Parts/Parser/Error.hs
+++ src/UHF/Parts/Parser/Error.hs
@@ -8,6 +8,7 @@ module UHF.Parts.Parser.Error
 import UHF.Prelude
 
 import UHF.Source.EqIgnoringSpans
+import UHF.Source.Span (Span)
 import UHF.Source.Located (Located (Located))
 import qualified UHF.Data.Token as Token
 import qualified UHF.Diagnostic as Diagnostic
@@ -15,6 +16,7 @@ import qualified UHF.Source.Located as Located
 
 data Error
     = BadToken Int Token.LToken Token.TokenType Text
+    | NotAPath Span
     -- | NoneMatched Token.LToken [Error]
     deriving (Generic, EqIgnoringSpans, Show, Eq)
 
@@ -24,7 +26,10 @@ instance Diagnostic.ToError (Located [Error]) where
             (Just sp)
             "parse error" -- TODO
             (map
-                (\ (BadToken _ tok expectation construct) ->
-                    Located.just_span tok `Diagnostic.msg_error_at` convert_str (construct <> " expects " <> format expectation <> " but got " <> format (Located.unlocate tok)))
-                bits) -- TODO: make this better
+                (\case
+                    BadToken _ tok expectation construct -> Located.just_span tok `Diagnostic.msg_error_at` convert_str (construct <> " expects " <> format expectation <> " but got " <> format (Located.unlocate tok))
+                    NotAPath sp -> sp `Diagnostic.msg_error_at` "not a path"
+                )
+                bits
+            ) -- TODO: make this better
             []

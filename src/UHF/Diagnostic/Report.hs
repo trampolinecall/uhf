d159422fba71461f2da1cdd030d4182793e6621e
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-29 20:29:06 -0500

    remove more unnecessary imports

--- src/UHF/Diagnostic/Report.hs
+++ src/UHF/Diagnostic/Report.hs
@@ -2,14 +2,12 @@ module UHF.Diagnostic.Report (DiagnosticStyle (..)) where
 
 import UHF.Prelude
 
-import qualified System.IO as IO
-
 import qualified UHF.Diagnostic.Diagnostic as Diagnostic
 import qualified UHF.Source.FormattedString as FormattedString
 
 class DiagnosticStyle s where
-    report_error          :: Diagnostic.ToError   e => s -> FormattedString.ColorsNeeded -> IO.Handle -> e                        -> IO ()
-    report_warning        :: Diagnostic.ToWarning w => s -> FormattedString.ColorsNeeded -> IO.Handle -> w                        -> IO ()
-    report_debug_message  ::                           s -> FormattedString.ColorsNeeded -> IO.Handle -> Diagnostic.DebugMessage  -> IO ()
-    report_internal_error ::                           s -> FormattedString.ColorsNeeded -> IO.Handle -> Diagnostic.InternalError -> IO ()
+    report_error          :: Diagnostic.ToError   e => s -> FormattedString.ColorsNeeded -> Handle -> e                        -> IO ()
+    report_warning        :: Diagnostic.ToWarning w => s -> FormattedString.ColorsNeeded -> Handle -> w                        -> IO ()
+    report_debug_message  ::                           s -> FormattedString.ColorsNeeded -> Handle -> Diagnostic.DebugMessage  -> IO ()
+    report_internal_error ::                           s -> FormattedString.ColorsNeeded -> Handle -> Diagnostic.InternalError -> IO ()
 

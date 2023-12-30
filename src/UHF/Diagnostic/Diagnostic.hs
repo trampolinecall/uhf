defa3f074e92b2022a42aa7a161aa845a85ee559
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2023-12-29 20:18:19 -0500

    reorganize files to allow for new styles

--- src/UHF/Diagnostic/Diagnostic.hs
+++ src/UHF/Diagnostic/Diagnostic.hs
@@ -9,46 +9,48 @@ module UHF.Diagnostic.Diagnostic
     , ToError(..)
     , ToWarning(..)
 
+    , Section(..)
+
     , MessagesSection
     , Message
     , MessageType(..)
     , msg_error_at, msg_warning_at, msg_note_at, msg_hint_at
     , msg_error, msg_warning, msg_note, msg_hint
 
-    , OtherSection(..)
     ) where
 
 import UHF.Prelude
 
 import UHF.Source.Span (Span)
 
-data Error = Error (Maybe Span) Text MessagesSection [OtherSection]
-data Warning = Warning (Maybe Span) Text MessagesSection [OtherSection]
-data DebugMessage = DebugMessage (Maybe Span) Text MessagesSection [OtherSection]
-data InternalError = InternalError (Maybe Span) Text MessagesSection [OtherSection]
+-- diagnostic types {{{1
+data Error         = Error         (Maybe Span) Text MessagesSection [Section]
+data Warning       = Warning       (Maybe Span) Text MessagesSection [Section]
+data DebugMessage  = DebugMessage  (Maybe Span) Text MessagesSection [Section]
+data InternalError = InternalError (Maybe Span) Text MessagesSection [Section]
 
-class ToError e where to_error :: e -> Error
+class ToError   e where to_error   :: e -> Error
 class ToWarning w where to_warning :: w -> Warning
 
-instance ToError Error where to_error = identity
+instance ToError   Error   where to_error   = identity
 instance ToWarning Warning where to_warning = identity
-instance ToError Void where to_error = absurd
-instance ToWarning Void where to_warning = absurd
-
+instance ToError   Void    where to_error   = absurd
+instance ToWarning Void    where to_warning = absurd
+-- sections {{{1
+newtype Section
+    = Section'Messages MessagesSection
+-- messages section {{{2
 type MessagesSection = [Message]
 type Message = (Maybe Span, MessageType, Maybe Text)
 data MessageType = MsgError | MsgWarning | MsgNote | MsgHint deriving (Show, Eq)
 
 msg_error_at, msg_warning_at, msg_note_at, msg_hint_at :: Span -> Text -> Message
-msg_error_at s m = (Just s, MsgError, Just m)
+msg_error_at   s m = (Just s, MsgError  , Just m)
 msg_warning_at s m = (Just s, MsgWarning, Just m)
-msg_note_at s m = (Just s, MsgNote, Just m)
-msg_hint_at s m = (Just s, MsgHint, Just m)
+msg_note_at    s m = (Just s, MsgNote   , Just m)
+msg_hint_at    s m = (Just s, MsgHint   , Just m)
 msg_error, msg_warning, msg_note, msg_hint :: Text -> Message
-msg_error m = (Nothing, MsgError, Just m)
+msg_error   m = (Nothing, MsgError  , Just m)
 msg_warning m = (Nothing, MsgWarning, Just m)
-msg_note m = (Nothing, MsgNote, Just m)
-msg_hint m = (Nothing, MsgHint, Just m)
-
-newtype OtherSection
-    = Section'Messages MessagesSection
+msg_note    m = (Nothing, MsgNote   , Just m)
+msg_hint    m = (Nothing, MsgHint   , Just m)
--- src/UHF/Diagnostic/Styles/Default/Utils.hs
+++ src/UHF/Diagnostic/Styles/Default/Utils.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module UHF.Diagnostic.Report.Utils where
+module UHF.Diagnostic.Styles.Default.Utils where
 
 import UHF.Prelude
 
@@ -11,8 +11,8 @@ import qualified Data.Text as Text
 import UHF.Source.File (File)
 import UHF.Source.Location.SpanHelper
 import UHF.Source.Span (Span)
-import qualified UHF.Diagnostic.Report.Line as Line
-import qualified UHF.Diagnostic.Report.Style as Style
+import qualified UHF.Diagnostic.Styles.Default.Line as Line
+import qualified UHF.Diagnostic.Styles.Default.Options as Options
 import qualified UHF.Source.File as File
 import qualified UHF.Source.Span as Span
 
@@ -46,7 +46,7 @@ flnr_comparator (f1, n1) (f2, n2)
 get_quote :: File -> Int -> Text
 get_quote fl nr = headDef "" $ drop (nr - 1) $ Text.lines $ File.contents fl
 
-file_and_elipsis_lines :: Style.Style -> Maybe (File, Int) -> (File, Int) -> [Line.Line]
+file_and_elipsis_lines :: Options.Options -> Maybe (File, Int) -> (File, Int) -> [Line.Line]
 file_and_elipsis_lines style (Just (lastf, lastn)) (curf, curn)
     | lastf /= curf = [Line.file_line style curf]
     | lastn + 1 /= curn = [Line.elipsis_line style]
@@ -57,10 +57,10 @@ case_file_and_elipsis_lines :: Assertion
 case_file_and_elipsis_lines =
     make_spans ["ajfowiejf"] >>= \ (f1, _) ->
     make_spans ["aobjiwoiejfawoeijf"] >>= \ (f2, _) ->
-    ([Line.file_line Style.default_style f2] @=? file_and_elipsis_lines Style.default_style (Just (f1, 2)) (f2, 12)) >>
-    ([Line.file_line Style.default_style f2] @=? file_and_elipsis_lines Style.default_style Nothing (f2, 12)) >>
-    ([] @=? file_and_elipsis_lines Style.default_style (Just (f2, 12)) (f2, 13)) >>
-    ([Line.elipsis_line Style.default_style] @=? file_and_elipsis_lines Style.default_style (Just (f2, 13)) (f2, 20))
+    ([Line.file_line Options.ascii_options f2] @=? file_and_elipsis_lines Options.ascii_options (Just (f1, 2)) (f2, 12)) >>
+    ([Line.file_line Options.ascii_options f2] @=? file_and_elipsis_lines Options.ascii_options Nothing (f2, 12)) >>
+    ([] @=? file_and_elipsis_lines Options.ascii_options (Just (f2, 12)) (f2, 13)) >>
+    ([Line.elipsis_line Options.ascii_options] @=? file_and_elipsis_lines Options.ascii_options (Just (f2, 13)) (f2, 20))
 
 tests :: TestTree
 tests = $(testGroupGenerator)

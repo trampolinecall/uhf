39a8757806513ec3d2fa365a5c66466dcaaa266a
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2024-02-17 13:07:10 -0500

    fix test file

--- test/uhf.hs
+++ test/uhf.hs
@@ -2,26 +2,26 @@ module Main where
 
 import UHF.Prelude
 
-import qualified UHF.Util.Arena (tests)
-import qualified UHF.Diagnostic.Report.Messages (tests)
-import qualified UHF.Diagnostic.Report.Utils (tests)
-import qualified UHF.Source.Location (tests)
+import qualified UHF.Diagnostic.Styles.Default.RenderMessagesSection (tests)
+import qualified UHF.Diagnostic.Styles.Default.Utils (tests)
 import qualified UHF.PP.Precedence (tests)
-import qualified UHF.Phases.Lexer (tests)
-import qualified UHF.Phases.Parser (tests)
-import qualified UHF.Phases.Parser.PEG (tests)
+import qualified UHF.Parts.Lexer (tests)
+import qualified UHF.Parts.Parser (tests)
+import qualified UHF.Parts.Parser.PEG (tests)
+import qualified UHF.Source.Location (tests)
+import qualified UHF.Util.Arena (tests)
 
 main :: IO ()
 main =
     defaultMain $
         testGroup
             "tests"
-            [ UHF.Diagnostic.Report.Messages.tests
-            , UHF.Diagnostic.Report.Utils.tests
+            [ UHF.Diagnostic.Styles.Default.RenderMessagesSection.tests
+            , UHF.Diagnostic.Styles.Default.Utils.tests
             , UHF.PP.Precedence.tests
-            , UHF.Phases.Lexer.tests
-            , UHF.Phases.Parser.PEG.tests
-            , UHF.Phases.Parser.tests
+            , UHF.Parts.Lexer.tests
+            , UHF.Parts.Parser.PEG.tests
+            , UHF.Parts.Parser.tests
             , UHF.Source.Location.tests
             , UHF.Util.Arena.tests
             ]

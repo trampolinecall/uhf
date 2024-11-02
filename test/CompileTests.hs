e30e05d39b498530c504870f57d59e43e9094bd3
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2024-11-02 08:54:56 -0400

    add globbing for compile tests

--- test/CompileTests.hs
+++ test/CompileTests.hs
@@ -1,7 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module CompileTests (get_compile_tests) where
 
 import UHF.Prelude
 
-get_compile_tests :: IO TestTree
-get_compile_tests = pure $ testGroup "compile tests" [] -- TODO
+import qualified Language.Haskell.TH as TH
+import qualified System.FilePath.Glob as Glob
+import qualified System.FilePath as FilePath
+import qualified System.Directory as Directory
 
+get_compile_tests :: IO TestTree
+get_compile_tests = do
+    filepaths <- compile_test_filepaths
+    pure $ testGroup "compile tests" $ map make_test_case filepaths -- TODO
+
+compile_test_filepaths :: IO [FilePath]
+compile_test_filepaths = Glob.globDir1 compile_test_filepath_pattern compile_test_search_directory
+    where
+        compile_test_filepath_pattern = Glob.compile "**/*.uhf"
+
+        compile_test_search_directory =
+            $(do
+                dir <- TH.runIO Directory.getCurrentDirectory
+                filename <- TH.loc_filename <$> TH.location
+                TH.litE $ TH.stringL $ dir FilePath.</> filename
+             )
+
+make_test_case :: FilePath -> TestTree
+make_test_case test_path = testCase test_path _

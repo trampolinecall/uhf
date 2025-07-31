46775fde0e08f20f122da61e33fa9b5ae6ba33d3
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2025-07-31 12:03:10 -0400

    merge infer var namers and move to separate module

--- src/UHF/Data/IR/TypeWithInferVar/PP.hs
+++ src/UHF/Data/IR/TypeWithInferVar/PP.hs
@@ -1,170 +1,16 @@
-module UHF.Data.IR.TypeWithInferVar.PP
-    ( InferVarNamer
-    , run_infer_var_namer
-    , make_infer_var_name_messages
-    , pp_type
-    )
-    where
+module UHF.Data.IR.TypeWithInferVar.PP (pp_type) where
 
 import UHF.Prelude
-
-import qualified Data.Map as Map
 
 import UHF.Data.IR.TypeWithInferVar
 import UHF.Source.Located (Located (..))
 import qualified UHF.Data.IR.Type as Type
 import qualified UHF.Data.IR.Type.PP as Type.PP
-import qualified UHF.Diagnostic as Diagnostic
 import qualified UHF.PP as PP
 import qualified UHF.Util.Arena as Arena
+import qualified UHF.Data.IR.TypeWithInferVar.PP.InferVarNamer as InferVarNamer
 
-{-
-TODO: figure out what this infer var namer is doing and how it is different from the one below
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TemplateHaskell #-}
-
-module UHF.Parts.UnifiedFrontendSolver.SolveTypes.Error.InferVarNamer (InferVarNamer, run_infer_var_namer, name_infer_var, tests) where
-
-import UHF.Prelude
-
-import Data.Char (isAsciiUpper)
-import qualified Data.Map as Map
-import qualified Data.Text as Text
-import GHC.Enum (Enum (succ))
-
-import qualified UHF.Parts.UnifiedFrontendSolver.TypeSolver as TypeSolver
-
--- InferVarName's list is in reverse order to the order that the names are displayed
--- this is because the logic in inc_name splits a name into the rightmost digit and the rest of the digits, which matches the structure of a linked list in reverse
--- therefore, the rightmost digit is placed at the beginning of the list
-newtype InferVarName = InferVarName [Char]
-newtype InferVarNamer a = InferVarNamer (State (InferVarName, Map TypeSolver.InferVarKey Text) a) deriving (Functor, Applicative, Monad)
-
--- TODO: adjust these depending on what names are in scope
-
-run_infer_var_namer :: InferVarNamer a -> (a, Map TypeSolver.InferVarKey Text)
-run_infer_var_namer (InferVarNamer s) = let (r, (_, n)) = runState s (InferVarName "A", Map.empty) in (r, n)
-
-name_infer_var :: TypeSolver.InferVarKey -> InferVarNamer Text
-name_infer_var var = InferVarNamer $ state $
-    \ (cur_n, cache) ->
-        case Map.lookup var cache of
-            Just res -> (res, (cur_n, cache))
-            Nothing ->
-                let name = show_infer_var_name cur_n
-                in (name, (inc_name cur_n, Map.insert var name cache))
-
-show_infer_var_name :: InferVarName -> Text
-show_infer_var_name (InferVarName n) = Text.reverse $ Text.pack n
-
-{-
-    the sequence of the variables names goes
-    A
-    B
-    C
-    ...
-    Y
-    Z
-    AA
-    AB
-    AC
-    ...
-    AY
-    AZ
-    BA
-    BB
-    BC
-    ...
-    ZY
-    ZZ
-    AAA
-    ...
-
-    this functions contains the core of the logic implementing this pattern
-    when a name is incremented, there are two possibilities:
-
-    1. increment last digit
-
-       if the last digit is not 'Z', increment it to get the new last digit
-       for example, given the name 'DGA', increment the last digit and get 'DGB'
-
-    2. roll over increment
-
-       if the last digit is 'Z', 'increment' it to 'A' and recursively roll over to the next places
-       for example, given the name 'GBZ', change 'Z' to 'A' and recursively call the increment function on 'GB' to get 'GCA'
-
-       there are is a special case: incrementing an empty string results in 'A'
-
-       the case where there are multiple digits and they are all 'Z' is handled by the above logic because the above logic will recursively call increment on smaller sections of 'Z's until it tries to increment '', at which point it becomes 'A'
-       for example, incrementing 'ZZ' will try to increment 'Z', which will try to increment '', which in turn becomes 'A'
--}
-inc_name :: InferVarName -> InferVarName
-inc_name (InferVarName n) = InferVarName (inc n)
-    where
-        inc ('Z':rest) = 'A' : inc rest
-        inc (first:rest) = inc_char first : rest
-        inc [] = ['A']
-
-        inc_char :: Char -> Char
-        inc_char 'Z' = error "inc_char called with 'Z'"
-        inc_char ch | isAsciiUpper ch = succ ch
-        inc_char ch = error $ "inc_char should only be called with uppercase ascii letters; got " ++ show ch
-
-testcase :: [Char] -> [Char] -> Assertion
-testcase input expected =
-    let InferVarName result = inc_name (InferVarName input)
-    in expected @=? result
-
-case_normal_a, case_normal_b, case_z, case_normal_2_a, case_normal_2_b, case_rollover_a, case_rollover_b, case_rollover_z, case_all_zs :: Assertion
-case_normal_a = testcase "A" "B"
-case_normal_b = testcase "B" "C"
-case_z = testcase "Z" "AA"
-case_normal_2_a = testcase "AA" "BA"
-case_normal_2_b = testcase "BA" "CA"
-case_rollover_a = testcase "ZA" "AB"
-case_rollover_b = testcase "ZB" "AC"
-case_rollover_z = testcase "AZ" "BZ"
-case_all_zs = testcase "ZZ" "AAA"
-
-tests :: TestTree
-tests = $(testGroupGenerator)
--}
-
--- this will be fixed in the infer-var-namer branch
--- TODO: figure out a way to have this split into a separate file without creating a import loop
-type InferVarNamer = State (Int, Map InferVarKey Text)
-run_infer_var_namer :: InferVarNamer a -> (a, Map InferVarKey Text)
-run_infer_var_namer s = let (r, (_, n)) = runState s (1, Map.empty) in (r, n)
-make_infer_var_name_messages :: InferVarArena -> Map InferVarKey Text -> [Diagnostic.Message]
-make_infer_var_name_messages infer_vars names =
-    map
-        (\ (key, name) ->
-            let (InferVar infer_var_for_what _) = Arena.get infer_vars key
-                var_sp = infer_var_for_what_sp infer_var_for_what
-                var_name = infer_var_for_what_name infer_var_for_what
-             in var_sp `Diagnostic.msg_note_at` convert_str ("where '" <> name <> "' is the type of this " <> var_name))
-        (Map.toList names)
-name_infer_var :: InferVarKey -> InferVarNamer Text
-name_infer_var var = state $
-    \ (cur_n, cache) ->
-        case Map.lookup var cache of
-            Just res -> (res, (cur_n, cache))
-            Nothing -> let name = "T" <> show cur_n in (name, (cur_n + 1, Map.insert var name cache))
-                {- TODO: make this work correctly
-                -- print like a base-26 number system
-                -- goes 'A', 'B', 'C', ..., 'Y', 'Z', 'AA', 'AB', 'AC', ..., 'AY', 'AZ', 'BA', 'BB', 'BC', ...
-                let name = Text.pack $ map to_characters $ reverse $ make_name [] cur_n
-                in (name, (cur_n + 1, Map.insert var name cache))
-    where
-        make_name acc n
-            | n > 0 =
-                make_name ((n `mod` 26) : acc) (n `div` 26)
-            | otherwise = acc
-
-        to_characters n = chr $ 65 + n
-    -}
-
-pp_type :: Bool -> Arena.Arena (Type.ADT a) Type.ADTKey -> Arena.Arena (Type.TypeSynonym b) Type.TypeSynonymKey -> Arena.Arena Type.QuantVar Type.QuantVarKey -> InferVarArena -> Type -> InferVarNamer PP.Token -- TODO: put the arenas and things into a reader monad inside InferVarNamer?
+pp_type :: Bool -> Arena.Arena (Type.ADT a) Type.ADTKey -> Arena.Arena (Type.TypeSynonym b) Type.TypeSynonymKey -> Arena.Arena Type.QuantVar Type.QuantVarKey -> InferVarArena -> Type -> InferVarNamer.InferVarNamer PP.Token -- TODO: put the arenas and things into a reader monad inside InferVarNamer?
 pp_type name_infer_vars adts type_synonyms vars infer_vars = go
     where
         -- TODO: it is a bit inelegant to duplicate this with IR.Type.Type because then the pretty printing code (and a lot of the other code) is duplicated and you have remember to change it in both places
@@ -191,7 +37,7 @@ pp_type name_infer_vars adts type_synonyms vars infer_vars = go
         go (Type'InferVar infer_var) =
             case Arena.get infer_vars infer_var of
                 InferVar _ Fresh
-                    | name_infer_vars -> PP.String <$> name_infer_var infer_var
+                    | name_infer_vars -> PP.String <$> InferVarNamer.name_infer_var infer_var
                     | otherwise -> pure "_"
                 InferVar _ (Substituted other) -> go other
         go (Type'QuantVar var) =
---
+++ src/UHF/Data/IR/TypeWithInferVar/PP/InferVarNamer.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module UHF.Data.IR.TypeWithInferVar.PP.InferVarNamer (InferVarNamer, run_infer_var_namer, name_infer_var, make_infer_var_name_messages, tests) where
+
+import UHF.Prelude
+
+import Data.Char (isAsciiUpper)
+import qualified Data.Map as Map
+import qualified Data.Text as Text
+import GHC.Enum (Enum (succ))
+import UHF.Data.IR.TypeWithInferVar
+import qualified UHF.Diagnostic as Diagnostic
+import qualified UHF.Util.Arena as Arena
+
+-- InferVarName's list is in reverse order to the order that the names are displayed
+-- this is because the logic in inc_name splits a name into the rightmost digit and the rest of the digits, which matches the structure of a linked list in reverse
+-- therefore, the rightmost digit is placed at the beginning of the list
+newtype InferVarName = InferVarName [Char]
+newtype InferVarNamer a = InferVarNamer (State (InferVarName, Map InferVarKey Text) a) deriving (Functor, Applicative, Monad)
+
+-- TODO: adjust these depending on what names are in scope
+
+run_infer_var_namer :: InferVarNamer a -> (a, Map InferVarKey Text)
+run_infer_var_namer (InferVarNamer s) = let (r, (_, n)) = runState s (InferVarName "A", Map.empty) in (r, n)
+
+name_infer_var :: InferVarKey -> InferVarNamer Text
+name_infer_var var = InferVarNamer $
+    state $
+        \(cur_n, cache) ->
+            case Map.lookup var cache of
+                Just res -> (res, (cur_n, cache))
+                Nothing ->
+                    let name = show_infer_var_name cur_n
+                    in (name, (inc_name cur_n, Map.insert var name cache))
+
+show_infer_var_name :: InferVarName -> Text
+show_infer_var_name (InferVarName n) = Text.reverse $ Text.pack n
+
+make_infer_var_name_messages :: InferVarArena -> Map InferVarKey Text -> [Diagnostic.Message]
+make_infer_var_name_messages infer_vars names =
+    map
+        ( \(key, name) ->
+            let (InferVar infer_var_for_what _) = Arena.get infer_vars key
+                var_sp = infer_var_for_what_sp infer_var_for_what
+                var_name = infer_var_for_what_name infer_var_for_what
+            in var_sp `Diagnostic.msg_note_at` convert_str ("where '" <> name <> "' is the type of this " <> var_name)
+        )
+        (Map.toList names)
+
+{-
+    the sequence of the variables names goes
+    A
+    B
+    C
+    ...
+    Y
+    Z
+    AA
+    AB
+    AC
+    ...
+    AY
+    AZ
+    BA
+    BB
+    BC
+    ...
+    ZY
+    ZZ
+    AAA
+    ...
+
+    this functions contains the core of the logic implementing this pattern
+    when a name is incremented, there are two possibilities:
+
+    1. increment last digit
+
+       if the last digit is not 'Z', increment it to get the new last digit
+       for example, given the name 'DGA', increment the last digit and get 'DGB'
+
+    2. roll over increment
+
+       if the last digit is 'Z', 'increment' it to 'A' and recursively roll over to the next places
+       for example, given the name 'GBZ', change 'Z' to 'A' and recursively call the increment function on 'GB' to get 'GCA'
+
+       there are is a special case: incrementing an empty string results in 'A'
+
+       the case where there are multiple digits and they are all 'Z' is handled by the above logic because the above logic will recursively call increment on smaller sections of 'Z's until it tries to increment '', at which point it becomes 'A'
+       for example, incrementing 'ZZ' will try to increment 'Z', which will try to increment '', which in turn becomes 'A'
+-}
+inc_name :: InferVarName -> InferVarName
+inc_name (InferVarName n) = InferVarName (inc n)
+    where
+        inc ('Z' : rest) = 'A' : inc rest
+        inc (first : rest) = inc_char first : rest
+        inc [] = ['A']
+
+        inc_char :: Char -> Char
+        inc_char 'Z' = error "inc_char called with 'Z'"
+        inc_char ch | isAsciiUpper ch = succ ch
+        inc_char ch = error $ "inc_char should only be called with uppercase ascii letters; got " ++ show ch
+
+testcase :: [Char] -> [Char] -> Assertion
+testcase input expected =
+    let InferVarName result = inc_name (InferVarName input)
+    in expected @=? result
+
+case_normal_a, case_normal_b, case_z, case_normal_2_a, case_normal_2_b, case_rollover_a, case_rollover_b, case_rollover_z, case_all_zs :: Assertion
+case_normal_a = testcase "A" "B"
+case_normal_b = testcase "B" "C"
+case_z = testcase "Z" "AA"
+case_normal_2_a = testcase "AA" "BA"
+case_normal_2_b = testcase "BA" "CA"
+case_rollover_a = testcase "ZA" "AB"
+case_rollover_b = testcase "ZB" "AC"
+case_rollover_z = testcase "AZ" "BZ"
+case_all_zs = testcase "ZZ" "AAA"
+
+tests :: TestTree
+tests = $(testGroupGenerator)
